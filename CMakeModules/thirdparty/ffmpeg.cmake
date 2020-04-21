cdec1b1dc5699c0133b9a47bde1527d6644f86c6
zippanto <zippanto@gmail.com>
2020-04-21 13:53:32 +0100

    OSX

--- CMakeModules/thirdparty/ffmpeg.cmake
+++ CMakeModules/thirdparty/ffmpeg.cmake
@@ -1,8 +1,8 @@
 # hack to be able to create a convenience library without sources
 file(WRITE ${LIB_DIR}/ffmpeg.c "")
 add_library(
-ffmpeg STATIC
-${LIB_DIR}/ffmpeg.c
+    ffmpeg STATIC
+    ${LIB_DIR}/ffmpeg.c
 )
 
 if (WIN32)
@@ -14,19 +14,19 @@ set(FFMPEG_LIB_DIR "${FFMPEG_DIR}/lib")
 set(FFMPEG_INCLUDE_DIR "${FFMPEG_DIR}/include")
 
 target_link_libraries(
-ffmpeg
-"${FFMPEG_LIB_DIR}/avcodec.lib"
-"${FFMPEG_LIB_DIR}/avdevice.lib"
-"${FFMPEG_LIB_DIR}/avfilter.lib"
-"${FFMPEG_LIB_DIR}/avformat.lib"
-"${FFMPEG_LIB_DIR}/avutil.lib"
-"${FFMPEG_LIB_DIR}/postproc.lib"
-"${FFMPEG_LIB_DIR}/swresample.lib"
-"${FFMPEG_LIB_DIR}/swscale.lib"
+    ffmpeg
+    "${FFMPEG_LIB_DIR}/avcodec.lib"
+    "${FFMPEG_LIB_DIR}/avdevice.lib"
+    "${FFMPEG_LIB_DIR}/avfilter.lib"
+    "${FFMPEG_LIB_DIR}/avformat.lib"
+    "${FFMPEG_LIB_DIR}/avutil.lib"
+    "${FFMPEG_LIB_DIR}/postproc.lib"
+    "${FFMPEG_LIB_DIR}/swresample.lib"
+    "${FFMPEG_LIB_DIR}/swscale.lib"
 )
 
 include_directories(
-"${FFMPEG_INCLUDE_DIR}"
+    "${FFMPEG_INCLUDE_DIR}"
 )
 
 ADD_GLOBAL_DEPENDENCY("${FFMPEG_BIN_DIR}/avcodec-58.dll")
@@ -43,11 +43,11 @@ find_package(FFmpeg COMPONENTS AVCODEC AVFORMAT SWSCALE REQUIRED)
 target_link_libraries(ffmpeg "${FFMPEG_LIBRARIES}")
 
 if (APPLE)
-include_directories(
-"/usr/local/include"
-)
-find_library(AVUTIL_LIBRARY avutil)
-find_library(SWRESAMPLE_LIBRARY swresample)
+    include_directories(
+            "/usr/local/include"
+    )
+    find_library(AVUTIL_LIBRARY avutil)
+    find_library(SWRESAMPLE_LIBRARY swresample)
 endif()
 
 endif()
