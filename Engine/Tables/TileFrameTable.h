faa2617a3cfac90e8b9e4c4b29c02125bfbb49f2
zippanto <zippanto@gmail.com>
2020-04-24 15:24:09 +0100

    TileTable::FromFileTxt refactor

--- Engine/Tables/TileFrameTable.h
+++ Engine/Tables/TileFrameTable.h
@@ -5,7 +5,8 @@
 
 /*  327 */
 enum TILE_DESC_FLAGS {
-    TILE_DESC_BURNS = 0x1,
+    TILE_DESC_NULL = 0,
+    TILE_DESC_BURN = 0x1,
     TILE_DESC_WATER = 0x2,
     TILE_DESC_BLOCK = 0x4,
     TILE_DESC_REPULSE = 0x8,
@@ -17,7 +18,35 @@ enum TILE_DESC_FLAGS {
     TILE_DESC_SCROLL_DOWN = 0x400,
     TILE_DESC_SCROLL_UP = 0x800,
     TILE_DESC_SCROLL_LEFT = 0x1000,
-    TILE_DESC_SCROLL_RIGHT = 0x2000,
+    TILE_DESC_SCROLL_RIGHT = 0x2000
+};
+
+enum TILE_SECT_FLAGS {
+    TILE_SECT_NULL = -1,
+    TILE_SECT_Start = -2,
+    TILE_SECT_Base1 = 0,
+    TILE_SECT_Base2_NS = 1,
+    TILE_SECT_Base3_EW = 2,
+    TILE_SECT_Base4_N_E = 3,
+    TILE_SECT_Special1_N_W = 4,
+    TILE_SECT_Special2_S_E = 5,
+    TILE_SECT_Special3_S_W = 6,
+    TILE_SECT_Special4_NS_E = 7,
+    TILE_SECT_Special5_NS_W = 8,
+    TILE_SECT_Special6_EW_N = 9,
+    TILE_SECT_Special7_EW_S = 0xA,
+    TILE_SECT_Special8_NCAP = 0xB,
+    TILE_SECT_NE1_SE1_ECAP = 0xC,
+    TILE_SECT_SCAP = 0xD,
+    TILE_SECT_NW1_SW1_WCAP = 0xE,
+    TILE_SECT_DN = 0xF,
+    TILE_SECT_E1_DS = 0x10,
+    TILE_SECT_W1_DW = 0x11,
+    TILE_SECT_N1_DE = 0x12,
+    TILE_SECT_S1_DSW = 0x13,
+    TILE_SECT_XNE1_XSE1_DNE = 0x14,
+    TILE_SECT_DSE = 0x15,
+    TILE_SECT_XNW1_XSW1_DNW = 0x16
 };
 
 #pragma warning(push)
@@ -25,16 +54,31 @@ enum TILE_DESC_FLAGS {
 enum Tileset : int16_t {
     Tileset_Grass = 0,
     Tileset_Snow = 1,
-    Tilset_Desert = 2,
+    Tileset_Desert = 2,
     Tileset_CooledLava = 3,
     Tileset_Dirt = 4,
     Tileset_Water = 5,
     Tileset_Badlands = 6,
     Tileset_Swamp = 7,
-    Tileset_8 = 8,
-    Tileset_9 = 9,
+    Tileset_Tropical = 8,
+    Tileset_City = 9,
     Tileset_RoadGrassCobble = 10,
-    Tileset_NULL = -1
+    Tileset_RoadGrassDirt = 11,
+    Tileset_RoadSnowCobble = 12,
+    Tileset_RoadSnowDirt = 13,
+    Tileset_RoadSandCobble = 14,
+    Tileset_RoadSandDirt = 15,
+    Tileset_RoadVolcanoCobble = 16,
+    Tileset_RoadVolcanoDirt = 17,
+    Tileset_RoadCrackedCobble = 22,
+    Tileset_RoadCrackedDirt = 23,
+    Tileset_RoadSwampCobble = 24,
+    Tileset_RoadSwampDir = 25,
+    Tileset_RoadTropicalCobble = 26,
+    Tileset_RoadTropicalDirt = 27,
+    Tileset_RoadCityStone = 28,
+    Tileset_NULL = -1,
+    Tileset_Start = -2
 };
 #pragma warning(pop)
 
@@ -48,7 +92,7 @@ class TileDesc {
     uint16_t uTileID = 0;
     Tileset tileset = Tileset_NULL;
     uint16_t uSection = 0;
-    uint16_t uAttributes = 0;
+    uint16_t uAttributes = TILE_DESC_NULL;
 
     inline Texture *GetTexture() {
         if (!this->texture) {
