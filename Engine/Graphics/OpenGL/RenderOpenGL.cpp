ff3c6099a30cd83e631a2738d73f9ce5e1efc1f8
zippanto <zippanto@gmail.com>
2020-05-26 22:20:05 +0100

    fix displaying screenshots

--- Engine/Graphics/OpenGL/RenderOpenGL.cpp
+++ Engine/Graphics/OpenGL/RenderOpenGL.cpp
@@ -2578,9 +2578,7 @@ bool RenderOpenGL::MoveTextureToDevice(Texture *texture) {
         // native_format == IMAGE_FORMAT_A1R5G5B5 ? GL_RGBA : GL_RGB;
 
     unsigned __int8 *pixels = nullptr;
-    if (native_format == IMAGE_FORMAT_R5G6B5) {
-        pixels = (unsigned __int8 *)t->GetPixels(IMAGE_FORMAT_R8G8B8);
-    } else if (native_format == IMAGE_FORMAT_A1R5G5B5 || native_format == IMAGE_FORMAT_A8R8G8B8) {
+    if (native_format == IMAGE_FORMAT_R5G6B5 || native_format == IMAGE_FORMAT_A1R5G5B5 || native_format == IMAGE_FORMAT_A8R8G8B8) {
         pixels = (unsigned __int8 *)t->GetPixels(IMAGE_FORMAT_R8G8B8A8);  // rgba
         gl_format = GL_RGBA;
     } else {
