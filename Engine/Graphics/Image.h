eb666aff0f5adbfd61be32268b011970250d8711
Urist <73941350+captainurist@users.noreply.github.com>
2021-12-24 15:23:30 +0300

    Docs

--- Engine/Graphics/Image.h
+++ Engine/Graphics/Image.h
@@ -81,8 +81,8 @@ struct TextureHeader {
     uint32_t uTextureSize;
     uint16_t uTextureWidth;
     uint16_t uTextureHeight;
-    int16_t uWidthLn2;
-    int16_t uHeightLn2;
+    int16_t uWidthLn2;  // log2(uTextureWidth)
+    int16_t uHeightLn2;  // log2(uTextureHeight)
     int16_t uWidthMinus1;
     int16_t uHeightMinus1;
     int16_t palette_id1;
@@ -90,7 +90,7 @@ struct TextureHeader {
     uint32_t uDecompressedSize;
     uint32_t pBits;  // 0x0002 - generate mipmaps
                      // 0x0200 - 0th palette entry is transparent, else colorkey
-                     // (7FF)
+                     // 0x0400 - don't free buffers (???)
 };
 #pragma pack(pop)
 
