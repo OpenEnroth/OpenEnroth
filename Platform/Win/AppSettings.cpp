e8ee3682bb05dc4eda86b56be6a569c1590e0693
Urist <captainurist@gmail.com>
2022-01-25 22:53:16 +0300

    Using iequals instead of strcmpi

--- Platform/Win/AppSettings.cpp
+++ Platform/Win/AppSettings.cpp
@@ -1,5 +1,7 @@
 #include "Platform/Win/Win.h"
 
+#include "Engine/Strings.h"
+
 bool OS_GetAppStringRecursive(
     HKEY parent_key,
     const char *path,
@@ -18,15 +20,15 @@ bool OS_GetAppStringRecursive(
         strcpy(path_tail, delimiter + 1);
 
         if (!parent_key) {
-            if (!strcmpi(current_key, "HKEY_CLASSES_ROOT"))
+            if (iequals(current_key, "HKEY_CLASSES_ROOT"))
                 parent_key = HKEY_CLASSES_ROOT;
-            else if (!strcmpi(current_key, "HKEY_CURRENT_CONFIG"))
+            else if (iequals(current_key, "HKEY_CURRENT_CONFIG"))
                 parent_key = HKEY_CURRENT_CONFIG;
-            else if (!strcmpi(current_key, "HKEY_CURRENT_USER"))
+            else if (iequals(current_key, "HKEY_CURRENT_USER"))
                 parent_key = HKEY_CURRENT_USER;
-            else if (!strcmpi(current_key, "HKEY_LOCAL_MACHINE"))
+            else if (iequals(current_key, "HKEY_LOCAL_MACHINE"))
                 parent_key = HKEY_LOCAL_MACHINE;
-            else if (!strcmpi(current_key, "HKEY_USERS"))
+            else if (iequals(current_key, "HKEY_USERS"))
                 parent_key = HKEY_USERS;
             else
                 return false;
