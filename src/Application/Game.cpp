ca717e50d6d088c51a32aa927f9b79edd210e85d
captainurist <captainurist@gmail.com>
2022-08-19 14:32:24 +0200

    Don't crash on debug actions when there is no active character

--- src/Application/Game.cpp
+++ src/Application/Game.cpp
@@ -2407,6 +2407,8 @@ void Game::EventLoop() {
 
                     break;
                 case UIMSG_DebugSpecialItem:
+                    if (uActiveCharacter == 0)
+                        continue;
                     pItemID = rand() % 500;
                     for (uint i = 0; i < 500; ++i) {
                         if (pItemID + i > 499) pItemID = 0;
@@ -2418,6 +2420,8 @@ void Game::EventLoop() {
                     pAudioPlayer->PlaySound(SOUND_StartMainChoice02, 0, 0, -1, 0, 0);
                     continue;
                 case UIMSG_DebugGenItem:
+                    if (uActiveCharacter == 0)
+                        continue;
                     pItemID = rand() % 500;
                     for (uint i = 0; i < 500; ++i) {
                         if (pItemID + i > 499) pItemID = 0;
@@ -2430,12 +2434,18 @@ void Game::EventLoop() {
                     pAudioPlayer->PlaySound(SOUND_StartMainChoice02, 0, 0, -1, 0, 0);
                     continue;
                 case UIMSG_DebugKillChar:
+                    if (uActiveCharacter == 0)
+                        continue;
                     pPlayers[uActiveCharacter]->SetCondition(Condition_Dead, 0);
                     continue;
                 case UIMSG_DebugEradicate:
+                    if (uActiveCharacter == 0)
+                        continue;
                     pPlayers[uActiveCharacter]->SetCondition(Condition_Eradicated, 0);
                     continue;
                 case UIMSG_DebugFullHeal:
+                    if (uActiveCharacter == 0)
+                        continue;
                     pPlayers[uActiveCharacter]->conditions_times.fill(GameTime(0));
                     pPlayers[uActiveCharacter]->sHealth =
                         pPlayers[uActiveCharacter]->GetMaxHealth();
@@ -2652,11 +2662,11 @@ void Game::EventLoop() {
                     continue;
                 case UIMSG_DebugGiveSkillP:
                     for (uint i = 0; i < 4; ++i) pParty->pPlayers[i].uSkillPoints += 50;
-                    pPlayers[uActiveCharacter]->PlayAwardSound_Anim();
+                    pPlayers[std::max(uActiveCharacter, 1u)]->PlayAwardSound_Anim();
                     continue;
                 case UIMSG_DebugGiveEXP:
                     pParty->GivePartyExp(20000);
-                    pPlayers[uActiveCharacter]->PlayAwardSound_Anim();
+                    pPlayers[std::max(uActiveCharacter, 1u)]->PlayAwardSound_Anim();
                     continue;
                 case UIMSG_DebugGiveGold:
                     Party::AddGold(10000);
