63fccff94e8e3adb5b140ce12383f3ead0007f64
captainurist <captainurist@gmail.com>
2022-11-14 17:56:16 +0800

    Rich Rect interface

--- src/Utility/Geometry/Rect.h
+++ src/Utility/Geometry/Rect.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "Point.h"
+#include "Size.h"
+
 template<class T>
 struct Rect {
     T x = 0;
@@ -9,6 +12,22 @@ struct Rect {
 
     Rect() = default;
     Rect(T x, T y, T w, T h): x(x), y(y), w(w), h(h) {}
+
+    bool Contains(const Point<T> &point) {
+        return x <= point.x && point.x < x + w && y <= point.y && point.y < y + h;
+    }
+
+    Point<T> TopLeft() const {
+        return {x, y};
+    }
+
+    Point<T> BottomRight() const {
+        return {x + w, y + h};
+    }
+
+    Size<T> Size() const {
+        return {w, h};
+    }
 };
 
 using Recti = Rect<int>;
