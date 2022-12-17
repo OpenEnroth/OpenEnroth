85fb2b096b71b798e165ecb45b21925c60ba629b
captainurist <73941350+captainurist@users.noreply.github.com>
2022-12-17 20:58:12 +0800

    _strrev -> std::reverse

--- src/Platform/Posix/Posix.h
+++ src/Platform/Posix/Posix.h
@@ -6,20 +6,5 @@
 
 #define _strnicmp strncasecmp
 
-inline char *_strrev(char *str) {
-    if (!str || !*str)
-        return str;
-
-    char ch;
-    int i = strlen(str) - 1, j = 0;
-    while (i > j) {
-        ch = str[i];
-        str[i] = str[j];
-        str[j] = ch;
-        i--;
-        j++;
-    }
-    return str;
-}
 
 
