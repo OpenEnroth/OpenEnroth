c90ebc60a651e6bde86c6be7294e664a9edc2253
captainurist <73941350+captainurist@users.noreply.github.com>
2022-12-17 18:35:01 +0800

    Cleaned up Posix.cpp

--- src/Platform/Posix/Posix.cpp
+++ src/Platform/Posix/Posix.cpp
@@ -1,22 +1,6 @@
 #include "Platform/Posix/Posix.h"
 
-#include <SDL.h>
-#include <dirent.h>
-#include <glob.h>
-#include <fnmatch.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-
-#include <string>
-#include <cstring>
-#include <sstream>
-#include <vector>
-#include <filesystem>
-
-#include "Utility/String.h"
 #include "Platform/Sdl/SdlPlatform.h"
-
-//////////////////// There is no Windows Registry ////////////////////
 
 std::unique_ptr<Platform> Platform::CreateStandardPlatform(Log *log) {
     return std::make_unique<SdlPlatform>(log);
