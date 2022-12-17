90117a36630561801d1180eb62bd2cb4b03160a3
captainurist <73941350+captainurist@users.noreply.github.com>
2022-12-17 15:32:04 +0800

    __VA_OPT__ should be available on all compilers that we support

--- src/Engine/ErrorHandling.h
+++ src/Engine/ErrorHandling.h
@@ -9,16 +9,8 @@
         exit(0);                                                    \
     } while (0)
 
-#ifdef _WINDOWS
-#define Assert(condition, ...)                                      \
-    Assert_impl_(__FILE__, __FUNCTION__, __LINE__, condition, #condition, __VA_ARGS__)
-#elif __APPLE__
-#define Assert(condition, ...)                                      \
-    Assert_impl_(__FILE__, __FUNCTION__, __LINE__, condition, #condition, ##__VA_ARGS__)
-#else
 #define Assert(condition, ...)                                      \
     Assert_impl_(__FILE__, __FUNCTION__, __LINE__, condition, #condition __VA_OPT__(,) __VA_ARGS__)  //NOLINT
-#endif
 
 void Error_impl_(const char *filename, const char *functionname,
                  int line, const char *format, ...);
