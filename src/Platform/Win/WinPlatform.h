25e2863f862632a21b30ac0bc70860b1c8b494e6
captainurist <73941350+captainurist@users.noreply.github.com>
2022-12-18 11:06:36 +0800

    More style

--- src/Platform/Win/WinPlatform.h
+++ src/Platform/Win/WinPlatform.h
@@ -5,7 +5,7 @@
 #include "Platform/Sdl/SdlPlatform.h"
 
 class WinPlatform : public SdlPlatform {
-public:
+ public:
     WinPlatform(Log *log);
 
     virtual void WinEnsureConsole() const override;
