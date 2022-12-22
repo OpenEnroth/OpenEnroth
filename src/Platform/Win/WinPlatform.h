899b75fa917719ab847635a9943ae380761e3d46
captainurist <73941350+captainurist@users.noreply.github.com>
2022-12-22 14:04:51 +0800

    Also fix style for Windows-only files

--- src/Platform/Win/WinPlatform.h
+++ src/Platform/Win/WinPlatform.h
@@ -6,7 +6,7 @@
 
 class WinPlatform : public SdlPlatform {
  public:
-    WinPlatform(Log *log);
+    explicit WinPlatform(Log *log);
 
     virtual void WinEnsureConsole() const override;
     virtual std::string WinQueryRegistry(const std::string &path) const override;
