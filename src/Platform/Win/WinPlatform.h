8ae169fbab65c6787e271e5ce1dd5ffc5a1fa6b4
captainurist <73941350+captainurist@users.noreply.github.com>
2022-12-23 16:46:54 +0800

    Windows build

--- src/Platform/Win/WinPlatform.h
+++ src/Platform/Win/WinPlatform.h
@@ -6,7 +6,7 @@
 
 class WinPlatform : public SdlPlatform {
  public:
-    explicit WinPlatform(Log *log);
+    explicit WinPlatform(PlatformLogLevel platformLogLevel);
 
     virtual std::string WinQueryRegistry(const std::string &path) const override;
 };
