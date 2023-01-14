b10bba753300fcbafcead394c127b9bb3a0aa2f1
captainurist <73941350+captainurist@users.noreply.github.com>
2023-01-14 08:16:20 +0800

    Docs for ProxyBase.

--- src/Platform/Proxy/ProxyBase.h
+++ src/Platform/Proxy/ProxyBase.h
@@ -2,6 +2,15 @@
 
 #include <cassert>
 
+/**
+ * A convenient base class for other platform proxies. It provides common accessors for the base class,
+ * and exposes a `NonNullBase` method for derived classes to simplify implementations.
+ *
+ * Note that it's perfectly OK for a proxy to point to a null base, and in this case `Base` will return `nullptr`,
+ * but `NonNullBase` will assert, and thus you cannot really call any methods on such a proxy.
+ *
+ * @tparam T                            Platform class to proxy, e.g. a `PlatformWindow`.
+ */
 template<class T>
 class ProxyBase : public T {
  public:
