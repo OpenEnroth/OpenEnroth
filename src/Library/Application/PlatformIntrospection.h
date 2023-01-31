b58a56abc7f86ec929b6bd16719920da05ea3e14
captainurist <73941350+captainurist@users.noreply.github.com>
2023-01-31 23:20:21 +0800

    Naming

--- src/Library/Application/PlatformIntrospection.h
+++ src/Library/Application/PlatformIntrospection.h
@@ -15,24 +15,24 @@
 class PlatformIntrospection {
  public:
     template<class T, class Callable>
-    static void visit(T *mixin, Callable &&callable) {
+    static void visit(T *plugin, Callable &&callable) {
         int count = 0;
         // Using += instead of one big expression to get deterministic call order.
-        count += visitInternal<ProxyPlatform>(mixin, std::forward<Callable>(callable));
-        count += visitInternal<ProxyEventLoop>(mixin, std::forward<Callable>(callable));
-        count += visitInternal<ProxyWindow>(mixin, std::forward<Callable>(callable));
-        count += visitInternal<ProxyOpenGLContext>(mixin, std::forward<Callable>(callable));
-        count += visitInternal<PlatformEventFilter>(mixin, std::forward<Callable>(callable));
-        count += visitInternal<PlatformApplicationAware>(mixin, std::forward<Callable>(callable));
+        count += visitInternal<ProxyPlatform>(plugin, std::forward<Callable>(callable));
+        count += visitInternal<ProxyEventLoop>(plugin, std::forward<Callable>(callable));
+        count += visitInternal<ProxyWindow>(plugin, std::forward<Callable>(callable));
+        count += visitInternal<ProxyOpenGLContext>(plugin, std::forward<Callable>(callable));
+        count += visitInternal<PlatformEventFilter>(plugin, std::forward<Callable>(callable));
+        count += visitInternal<PlatformApplicationAware>(plugin, std::forward<Callable>(callable));
         assert(count > 0);
         (void) count;
     }
 
  private:
     template<class Installable, class T, class Callable>
-    static bool visitInternal(T *mixin, Callable &&callable) {
+    static bool visitInternal(T *plugin, Callable &&callable) {
         if constexpr (std::is_base_of_v<Installable, T>) {
-            callable(static_cast<Installable *>(mixin));
+            callable(static_cast<Installable *>(plugin));
             return true;
         } else {
             return false;
