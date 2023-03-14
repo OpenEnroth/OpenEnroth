df10daaef9de53b388633f9afccac18b6053942b
Peter Skelton <peter.r.skelton@gmail.com>
2023-03-14 22:51:05 +0000

    add test for #521 and reorder

--- test/Tests/TestPartyCreationMenu.cpp
+++ test/Tests/TestPartyCreationMenu.cpp
@@ -18,6 +18,37 @@ GAME_TEST(Items, GenerateItem) {
         pItemTable->GenerateItem(ITEM_TREASURE_LEVEL_6, 0, &item);
 }
 
+// 100
+
+GAME_TEST(Issue, Issue123) {
+    // Party falls when flying
+    test->playTraceFromTestData("issue_123.mm7", "issue_123.json");
+    // check party is still in the air
+    EXPECT_GT(pParty->vPosition.z, 512);
+}
+
+static uint64_t GetPartyHealth() {
+    uint64_t result = 0;
+    for (const Player &player : pParty->pPlayers)
+        result += player.sHealth;
+    return result;
+}
+
+GAME_TEST(Issues, Issue125) {
+    // check that fireballs hurt party
+    engine->config->debug.AllMagic.Set(true);
+
+    uint64_t oldHealth = 0;
+    test->playTraceFromTestData("issue_125.mm7", "issue_125.json", [&] { oldHealth = GetPartyHealth(); });
+    uint64_t newHealth = GetPartyHealth();
+    EXPECT_LT(newHealth, oldHealth);
+}
+
+GAME_TEST(Issues, Issue159) {
+    // Exception when entering Tidewater Caverns
+    test->playTraceFromTestData("issue_159.mm7", "issue_159.json");
+}
+
 GAME_TEST(Issues, Issue163) {
     // Testing that pressing the Load Game button doesn't crash even if the 'saves' folder doesn't exist.
     std::string savesDir = MakeDataPath("saves");
@@ -28,7 +59,7 @@ GAME_TEST(Issues, Issue163) {
             std::error_code ec;
             std::filesystem::rename(savesDirMoved, savesDir, ec); // Using std::error_code here, so can't throw.
         }
-    });
+        });
 
     if (std::filesystem::exists(savesDir)) {
         savesDirMoved = savesDir + "_moved_for_testing";
@@ -68,21 +99,49 @@ GAME_TEST(Issues, Issue198) {
 
     // Preload item images in the main thread first.
     game->runGameRoutine([&] {
-        forEachInventoryItem([] (const ItemGen &item, int /*x*/, int /*y*/) {
+        forEachInventoryItem([](const ItemGen &item, int /*x*/, int /*y*/) {
             // Calling GetWidth forces the texture to be created.
             assets->GetImage_ColorKey(pItemTable->pItems[item.uItemID].pIconName)->GetWidth();
+            });
         });
-    });
 
     // Then can safely check everything.
-    forEachInventoryItem([] (const ItemGen &item, int x, int y) {
+    forEachInventoryItem([](const ItemGen &item, int x, int y) {
         Texture *image = assets->GetImage_ColorKey(pItemTable->pItems[item.uItemID].pIconName);
         int width = GetSizeInInventorySlots(image->GetWidth());
         int height = GetSizeInInventorySlots(image->GetHeight());
 
         EXPECT_LE(x + width, Player::INVENTORY_SLOTS_WIDTH);
         EXPECT_LE(y + height, Player::INVENTORY_SLOTS_HEIGHT);
-    });
+        });
+}
+
+// 200
+
+GAME_TEST(Issue, Issue201) {
+    // Unhandled EVENT_ShowMovie in Event Processor
+    uint64_t oldHealth = 0;
+    uint64_t oldgametime{};
+    test->playTraceFromTestData("issue_201.mm7", "issue_201.json", [&] { oldHealth = GetPartyHealth(); oldgametime = pParty->GetPlayingTime().GetDays(); });
+
+    // party should heal
+    uint64_t newHealth = GetPartyHealth();
+    EXPECT_GT(newHealth, oldHealth);
+    // we should be teleported to harmondale
+    EXPECT_EQ(pCurrentMapName, "Out02.odm");
+    // time should advance by a week
+    uint64_t newtime = pParty->GetPlayingTime().GetDays();
+    EXPECT_EQ((oldgametime + GameTime(0, 0, 0, 0, 1).GetDays()), newtime);
+}
+
+GAME_TEST(Issue, Issue202) {
+    //Judge doesn't move to house and stays with the party
+    int oldhirecount{};
+    test->playTraceFromTestData("issue_202.mm7", "issue_202.json", [&]() {oldhirecount = pParty->CountHirelings(); });
+    // judge shouldnt be with party anymore
+    EXPECT_EQ(oldhirecount - 1, pParty->CountHirelings());
+    // party align evil
+    EXPECT_EQ(pParty->alignment, PartyAlignment_Evil);
 }
 
 GAME_TEST(Issues, Issue203) {
@@ -90,22 +149,34 @@ GAME_TEST(Issues, Issue203) {
     test->playTraceFromTestData("issue_203.mm7", "issue_203.json");
 }
 
-GAME_TEST(Prs, Pr314) {
-    // Check that character creating menu works.
-    // Trace pretty much presses all the buttons and opens all the popups possible.
-    test->playTraceFromTestData("pr_314.mm7", "pr_314.json");
+GAME_TEST(Issue, Issue211) {
+    // Crash during accidental ok double click
+    test->playTraceFromTestData("issue_211.mm7", "issue_211.json");
+}
 
-    for (int i = 0; i < 4; i++)
-        EXPECT_EQ(pParty->pPlayers[i].uLuck, 20);
+static void check223res(CHARACTER_ATTRIBUTE_TYPE res, std::initializer_list<std::pair<int, int>> playerrespairs) {
+    for (auto pair : playerrespairs) {
+        EXPECT_EQ(pParty->pPlayers[pair.first].GetActualResistance(res), pair.second);
+    }
+}
 
-    EXPECT_EQ(pParty->pPlayers[0].classType, PLAYER_CLASS_MONK);
-    EXPECT_EQ(pParty->pPlayers[1].classType, PLAYER_CLASS_THIEF);
-    EXPECT_EQ(pParty->pPlayers[2].classType, PLAYER_CLASS_RANGER);
-    EXPECT_EQ(pParty->pPlayers[3].classType, PLAYER_CLASS_CLERIC);
-    EXPECT_EQ(pParty->pPlayers[0].GetRace(), CHARACTER_RACE_ELF);
-    EXPECT_EQ(pParty->pPlayers[1].GetRace(), CHARACTER_RACE_ELF);
-    EXPECT_EQ(pParty->pPlayers[2].GetRace(), CHARACTER_RACE_GOBLIN);
-    EXPECT_EQ(pParty->pPlayers[3].GetRace(), CHARACTER_RACE_ELF);
+GAME_TEST(Issue, Issue223) {
+    // Fire and air resistance not resetting between games
+    test->playTraceFromTestData("issue_223.mm7", "issue_223.json");
+    // expect normal resistances 55-00-00-00
+    check223res(CHARACTER_ATTRIBUTE_RESIST_FIRE, { {0, 5}, {1, 0}, {2, 0}, {3, 0} });
+    check223res(CHARACTER_ATTRIBUTE_RESIST_AIR, { {0, 5}, {1, 0}, {2, 0}, {3, 0} });
+}
+
+GAME_TEST(Issues, Issue268) {
+    // Crash in ODM_GetFloorLevel
+    test->playTraceFromTestData("issue_268.mm7", "issue_268.json");
+}
+
+GAME_TEST(Issues, Issue271) {
+    // Party shouldnt yell when landing from flight
+    test->playTraceFromTestData("issue_271.mm7", "issue_271.json");
+    EXPECT_NE(pParty->pPlayers[1].expression, CHARACTER_EXPRESSION_FEAR);
 }
 
 static int partyItemCount() {
@@ -193,6 +264,26 @@ GAME_TEST(Issues, Issue294) {
     // TODO(captainurist): ^fails now
 }
 
+// 300
+
+GAME_TEST(Prs, Pr314) {
+    // Check that character creating menu works.
+    // Trace pretty much presses all the buttons and opens all the popups possible.
+    test->playTraceFromTestData("pr_314.mm7", "pr_314.json");
+
+    for (int i = 0; i < 4; i++)
+        EXPECT_EQ(pParty->pPlayers[i].uLuck, 20);
+
+    EXPECT_EQ(pParty->pPlayers[0].classType, PLAYER_CLASS_MONK);
+    EXPECT_EQ(pParty->pPlayers[1].classType, PLAYER_CLASS_THIEF);
+    EXPECT_EQ(pParty->pPlayers[2].classType, PLAYER_CLASS_RANGER);
+    EXPECT_EQ(pParty->pPlayers[3].classType, PLAYER_CLASS_CLERIC);
+    EXPECT_EQ(pParty->pPlayers[0].GetRace(), CHARACTER_RACE_ELF);
+    EXPECT_EQ(pParty->pPlayers[1].GetRace(), CHARACTER_RACE_ELF);
+    EXPECT_EQ(pParty->pPlayers[2].GetRace(), CHARACTER_RACE_GOBLIN);
+    EXPECT_EQ(pParty->pPlayers[3].GetRace(), CHARACTER_RACE_ELF);
+}
+
 GAME_TEST(Issues, Issue315) {
     test->loadGameFromTestData("issue_315.mm7");
     game->goToMainMenu();
@@ -202,9 +293,9 @@ GAME_TEST(Issues, Issue315) {
     game->skipLoadingScreen(); // This shouldn't crash.
 }
 
-GAME_TEST(Issues, Issue403) {
-    // Entering Lincoln shouldn't crash.
-    test->playTraceFromTestData("issue_403.mm7", "issue_403.json");
+GAME_TEST(Issue, Issue331) {
+    // Assert when traveling by horse caused by out of bound access to pObjectList->pObjects
+    test->playTraceFromTestData("issue_331.mm7", "issue_331.json");
 }
 
 GAME_TEST(Prs, Pr347) {
@@ -228,9 +319,28 @@ GAME_TEST(Issues, Issue388) {
     pArcomageGame->_targetFPS = oldfpslimit;
 }
 
+static void check395exp(std::initializer_list<std::pair<int, int>> playerexppairs) {
+    for (auto pair : playerexppairs) {
+        EXPECT_EQ(pParty->pPlayers[pair.first].uExperience, pair.second);
+    }
+}
+
+GAME_TEST(Issue, Issue395) {
+    // Check that learning skill works as intended
+    test->playTraceFromTestData("issue_395.mm7", "issue_395.json", []() { check395exp({ {0, 100}, {1, 100}, {2, 100}, {3, 100} }); });
+    check395exp({ {0, 214}, {1, 228}, {2, 237}, {3, 258} });
+}
+
+// 400
+
 GAME_TEST(Issues, Issue402) {
     // Attacking while wearing wetsuits shouldn't assert.
     test->playTraceFromTestData("issue_402.mm7", "issue_402.json");
+}
+
+GAME_TEST(Issues, Issue403) {
+    // Entering Lincoln shouldn't crash.
+    test->playTraceFromTestData("issue_403.mm7", "issue_403.json");
 }
 
 GAME_TEST(Issues, Issue408) {
@@ -270,111 +380,25 @@ GAME_TEST(Issues, Issue427) {
     test->playTraceFromTestData("issue_427a.mm7", "issue_427a.json");
 
     // Check that spell targeting works correctly - 1st char is getting the buffs.
-    check427Buffs("a", {0}, true);
-    check427Buffs("a", {1, 2, 3}, false);
+    check427Buffs("a", { 0 }, true);
+    check427Buffs("a", { 1, 2, 3 }, false);
 
     // In this test mastery is enough for the whole party
     test->playTraceFromTestData("issue_427b.mm7", "issue_427b.json");
 
     // Check that all character have buffs
-    check427Buffs("b", {0, 1, 2, 3}, true);
+    check427Buffs("b", { 0, 1, 2, 3 }, true);
 }
 
-uint64_t GetPartyHealth() {
-    uint64_t result = 0;
-    for (const Player& player : pParty->pPlayers)
-        result += player.sHealth;
-    return result;
-}
-
-GAME_TEST(Issues, Issue125) {
-    // check that fireballs hurt party
-    engine->config->debug.AllMagic.Set(true);
-
-    uint64_t oldHealth = 0;
-    test->playTraceFromTestData("issue_125.mm7", "issue_125.json", [&] { oldHealth = GetPartyHealth(); });
-    uint64_t newHealth = GetPartyHealth();
-    EXPECT_LT(newHealth, oldHealth);
-}
-
-GAME_TEST(Issues, Issue159) {
-    // Exception when entering Tidewater Caverns
-    test->playTraceFromTestData("issue_159.mm7", "issue_159.json");
-}
-
-GAME_TEST(Issue, Issue123) {
-    // Party falls when flying
-    test->playTraceFromTestData("issue_123.mm7", "issue_123.json");
-    // check party is still in the air
-    EXPECT_GT(pParty->vPosition.z, 512);
-}
-
-GAME_TEST(Issue, Issue201) {
-    // Unhandled EVENT_ShowMovie in Event Processor
-    uint64_t oldHealth = 0;
-    uint64_t oldgametime{};
-    test->playTraceFromTestData("issue_201.mm7", "issue_201.json", [&] { oldHealth = GetPartyHealth(); oldgametime = pParty->GetPlayingTime().GetDays(); });
-
-    // party should heal
-    uint64_t newHealth = GetPartyHealth();
-    EXPECT_GT(newHealth, oldHealth);
-    // we should be teleported to harmondale
-    EXPECT_EQ(pCurrentMapName, "Out02.odm");
-    // time should advance by a week
-    uint64_t newtime = pParty->GetPlayingTime().GetDays();
-    EXPECT_EQ((oldgametime + GameTime(0, 0, 0, 0, 1).GetDays()), newtime);
-}
-
-GAME_TEST(Issue, Issue202) {
-    //Judge doesn't move to house and stays with the party
-    int oldhirecount{};
-    test->playTraceFromTestData("issue_202.mm7", "issue_202.json", [&]() {oldhirecount = pParty->CountHirelings(); });
-    // judge shouldnt be with party anymore
-    EXPECT_EQ(oldhirecount - 1, pParty->CountHirelings());
-    // party align evil
-    EXPECT_EQ(pParty->alignment, PartyAlignment_Evil);
-}
-
-GAME_TEST(Issue, Issue211) {
-    // Crash during accidental ok double click
-    test->playTraceFromTestData("issue_211.mm7", "issue_211.json");
-}
-
-
-static void check223res(CHARACTER_ATTRIBUTE_TYPE res, std::initializer_list<std::pair<int, int>> playerrespairs) {
-    for (auto pair : playerrespairs) {
-        EXPECT_EQ(pParty->pPlayers[pair.first].GetActualResistance(res), pair.second);
-    }
-}
-
-GAME_TEST(Issue, Issue223) {
-    // Fire and air resistance not resetting between games
-    test->playTraceFromTestData("issue_223.mm7", "issue_223.json");
-    // expect normal resistances 55-00-00-00
-    check223res(CHARACTER_ATTRIBUTE_RESIST_FIRE, { {0, 5}, {1, 0}, {2, 0}, {3, 0} });
-    check223res(CHARACTER_ATTRIBUTE_RESIST_AIR, { {0, 5}, {1, 0}, {2, 0}, {3, 0} });
+GAME_TEST(Issue, Issue442) {
+    // Test that regular UI is blocked on spell cast
+    test->playTraceFromTestData("issue_442.mm7", "issue_442.json");
+    EXPECT_EQ(pParty->pPlayers[1].pPlayerBuffs[PLAYER_BUFF_BLESS].Active(), true);
 }
 
 GAME_TEST(Prs, Pr469) {
     // Assert when using Quick Spell button when spell is not set
     test->playTraceFromTestData("pr_469.mm7", "pr_469.json");
-}
-
-GAME_TEST(Issue, Issue331) {
-    // Assert when traveling by horse caused by out of bound access to pObjectList->pObjects
-    test->playTraceFromTestData("issue_331.mm7", "issue_331.json");
-}
-
-static void check395exp(std::initializer_list<std::pair<int, int>> playerexppairs) {
-    for (auto pair : playerexppairs) {
-        EXPECT_EQ(pParty->pPlayers[pair.first].uExperience, pair.second);
-    }
-}
-
-GAME_TEST(Issue, Issue395) {
-    // Check that learning skill works as intended
-    test->playTraceFromTestData("issue_395.mm7", "issue_395.json", []() { check395exp({ {0, 100}, {1, 100}, {2, 100}, {3, 100} }); });
-    check395exp({ {0, 214}, {1, 228}, {2, 237}, {3, 258} });
 }
 
 GAME_TEST(Issue, Issue490) {
@@ -393,6 +417,8 @@ GAME_TEST(Issue, Issue492) {
     test->playTraceFromTestData("issue_492.mm7", "issue_492.json", []() { EXPECT_EQ(pParty->pPlayers[0].uExperience, 279); });
     EXPECT_EQ(pParty->pPlayers[0].uExperience, 287);
 }
+
+// 500
 
 GAME_TEST(Issue, Issue502) {
     // Check that script face animation and voice indexes right characters
@@ -422,6 +448,18 @@ GAME_TEST(Issue, Issue506) {
     test->playTraceFromTestData("issue_506.mm7", "issue_506.json");
 }
 
+GAME_TEST(Issues, Issue520) {
+    // Party should take fall damage
+    uint64_t oldHealth = 0;
+    test->playTraceFromTestData("issue_520.mm7", "issue_520.json", [&] { oldHealth = GetPartyHealth(); });
+    EXPECT_LT(GetPartyHealth(), oldHealth);
+}
+
+GAME_TEST(Issues, Issue521) {
+    // 500 endurance leads to asserts in Player::SetRecoveryTime
+    test->playTraceFromTestData("issue_521.mm7", "issue_521.json");
+}
+
 GAME_TEST(Issue, Issue527) {
     // Check Cure Disease spell works
     test->playTraceFromTestData("issue_527.mm7", "issue_527.json", []() { EXPECT_TRUE(pParty->pPlayers[0].conditions.Has(Condition_Disease_Weak)); });
@@ -435,29 +473,6 @@ GAME_TEST(Issue, Issue528) {
     EXPECT_EQ(pParty->pPlayers[2].sMana, 40);
 }
 
-GAME_TEST(Issue, Issue442) {
-    // Test that regular UI is blocked on spell cast
-    test->playTraceFromTestData("issue_442.mm7", "issue_442.json");
-    EXPECT_EQ(pParty->pPlayers[1].pPlayerBuffs[PLAYER_BUFF_BLESS].Active(), true);
-}
-
-GAME_TEST(Issues, Issue268) {
-    // Crash in ODM_GetFloorLevel
-    test->playTraceFromTestData("issue_268.mm7", "issue_268.json");
-}
-
-GAME_TEST(Issues, Issue271) {
-    // Party shouldnt yell when landing from flight
-    test->playTraceFromTestData("issue_271.mm7", "issue_271.json");
-    EXPECT_NE(pParty->pPlayers[1].expression, CHARACTER_EXPRESSION_FEAR);
-}
-
-GAME_TEST(Issues, Issue520) {
-    // Party should take fall damage
-    uint64_t oldHealth = 0;
-    test->playTraceFromTestData("issue_520.mm7", "issue_520.json", [&] { oldHealth = GetPartyHealth(); });
-    EXPECT_LT(GetPartyHealth(), oldHealth);
-}
 
 // This cant be tested properly using the current framework
 //GAME_TEST(Issues, Issue405) {
