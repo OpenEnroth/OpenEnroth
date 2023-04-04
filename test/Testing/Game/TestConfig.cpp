570f3ed342f2428bc8915242a22d6354bec45b93
Peter Skelton <peter.r.skelton@gmail.com>
2023-04-04 22:45:53 +0100

    turn sound off in tests

--- test/Testing/Game/TestConfig.cpp
+++ test/Testing/Game/TestConfig.cpp
@@ -5,6 +5,7 @@
 void ResetTestConfig(GameConfig *config) {
     config->reset();
 
+    config->debug.NoSound.setValue(true);
     config->debug.NoVideo.setValue(true);
     config->window.MouseGrab.setValue(false);
     config->graphics.FPSLimit.setValue(0); // Unlimited
