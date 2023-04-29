55692bfcf6c893f3120107d0c28e507341a8c598
captainurist <73941350+captainurist@users.noreply.github.com>
2023-04-29 01:16:20 +0000

    Deploying to gh-pages from @ OpenEnroth/OpenEnroth@9748626e6226cd1776ff31ab2b2a3b95e7d8cf9d 🚀

--- search/enums_0.js
+++ search/enums_0.js
@@ -2,10 +2,11 @@ var searchData=
 [
   ['ability_5findex_0',['ABILITY_INDEX',['../ActorEnums_8h.html#a224eb67ac920c1beeacc7956d5c95fa2',1,'ActorEnums.h']]],
   ['actor_5fbuff_5findex_1',['ACTOR_BUFF_INDEX',['../ActorEnums_8h.html#ae52eebc972ce5a2808557b0a16cc97e2',1,'ActorEnums.h']]],
-  ['actoranimation_2',['ActorAnimation',['../ActorEnums_8h.html#a00c665f8db82f6bfeaaa22bcff8e0ab7',1,'ActorEnums.h']]],
-  ['actorattribute_3',['ActorAttribute',['../ActorEnums_8h.html#a0449589ea79a89a7e564d1cf1b0a9b5f',1,'ActorEnums.h']]],
-  ['aistate_4',['AIState',['../ActorEnums_8h.html#a75a2677e7281dc615368b0814c395fed',1,'ActorEnums.h']]],
-  ['arcomagemessagetype_5',['ArcomageMessageType',['../Arcomage_8h.html#a56d38dcb51bcfc543b0d26aea6641232',1,'Arcomage.h']]],
-  ['autonote_5ftype_6',['AUTONOTE_TYPE',['../Autonotes_8h.html#ac492beabbce596a9d1992e7828d21b99',1,'Autonotes.h']]],
-  ['awardtype_7',['AwardType',['../Awards_8h.html#affb7937911d9f95cf70e26a581d989c7',1,'Awards.h']]]
+  ['actor_5fkill_5fcheck_5fpolicy_2',['ACTOR_KILL_CHECK_POLICY',['../ActorEnums_8h.html#abf626b8034b1662f854cd5d75b65f224',1,'ActorEnums.h']]],
+  ['actoranimation_3',['ActorAnimation',['../ActorEnums_8h.html#a00c665f8db82f6bfeaaa22bcff8e0ab7',1,'ActorEnums.h']]],
+  ['actorattribute_4',['ActorAttribute',['../ActorEnums_8h.html#a0449589ea79a89a7e564d1cf1b0a9b5f',1,'ActorEnums.h']]],
+  ['aistate_5',['AIState',['../ActorEnums_8h.html#a75a2677e7281dc615368b0814c395fed',1,'ActorEnums.h']]],
+  ['arcomagemessagetype_6',['ArcomageMessageType',['../Arcomage_8h.html#a56d38dcb51bcfc543b0d26aea6641232',1,'Arcomage.h']]],
+  ['autonote_5ftype_7',['AUTONOTE_TYPE',['../Autonotes_8h.html#ac492beabbce596a9d1992e7828d21b99',1,'Autonotes.h']]],
+  ['awardtype_8',['AwardType',['../Awards_8h.html#affb7937911d9f95cf70e26a581d989c7',1,'Awards.h']]]
 ];
--- search/enumvalues_a.js
+++ search/enumvalues_a.js
@@ -1,4 +1,8 @@
 var searchData=
 [
-  ['k_0',['K',['../PlatformEnums_8h.html#a2ba59747730ab559dd1191f51081eeccaa5f3c6a11b03839d46af9fb43c97c188',1,'PlatformEnums.h']]]
+  ['k_0',['K',['../PlatformEnums_8h.html#a2ba59747730ab559dd1191f51081eeccaa5f3c6a11b03839d46af9fb43c97c188',1,'PlatformEnums.h']]],
+  ['kill_5fcheck_5factorid_1',['KILL_CHECK_ACTORID',['../ActorEnums_8h.html#abf626b8034b1662f854cd5d75b65f224ab91fcd3ea12e4a5035ebacdbccc4979c',1,'ActorEnums.h']]],
+  ['kill_5fcheck_5fany_2',['KILL_CHECK_ANY',['../ActorEnums_8h.html#abf626b8034b1662f854cd5d75b65f224a9e31046d592fe3bbe2ab41f4dd89f782',1,'ActorEnums.h']]],
+  ['kill_5fcheck_5fgroupid_3',['KILL_CHECK_GROUPID',['../ActorEnums_8h.html#abf626b8034b1662f854cd5d75b65f224ad618be51c321932e9b5e4b07e12810ab',1,'ActorEnums.h']]],
+  ['kill_5fcheck_5fmonsterid_4',['KILL_CHECK_MONSTERID',['../ActorEnums_8h.html#abf626b8034b1662f854cd5d75b65f224ae984ad5a3492404c7497031fe01393eb',1,'ActorEnums.h']]]
 ];
