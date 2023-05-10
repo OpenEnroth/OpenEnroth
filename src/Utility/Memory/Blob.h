3fab3f680bf39fa36ea15535f4a8e8c4f43de1e5
captainurist <73941350+captainurist@users.noreply.github.com>
2023-05-10 11:59:40 +0800

    Saner implementation of Blob::subBlob

--- src/Utility/Memory/Blob.h
+++ src/Utility/Memory/Blob.h
@@ -2,21 +2,15 @@
 
 #include <cassert>
 #include <cstdlib>
-#include <array>
 #include <utility>
 #include <memory>
 #include <string_view>
 #include <string>
+#include <type_traits>
+
+#include "FreeDeleter.h"
 
 class FileInputStream;
-class Blob;
-
-class BlobHandler {
- public:
-    virtual ~BlobHandler() = default;
-    virtual void destroy(Blob &self) = 0;
-    virtual Blob share(Blob &self) = 0;
-};
 
 /**
  * `Blob` is an abstraction that couples a contiguous memory region with the knowledge of how to deallocate it.
@@ -28,25 +22,13 @@ class Blob final {
  public:
     Blob() {}
 
-    Blob(const void *data, size_t size, BlobHandler *handler) {
-        assert(data ? size > 0 && handler : true);
-        assert(!data ? size == 0 && !handler : true);
-
-        _data = data;
-        _size = size;
-        _handler = handler;
-    }
-
     Blob(const Blob &) = delete; // Blobs are non-copyable.
 
     Blob(Blob &&other) {
         swap(*this, other);
     }
 
-    ~Blob() {
-        if (_handler)
-            _handler->destroy(*this);
-    }
+    ~Blob() = default;
 
     Blob &operator=(const Blob &) = delete; // Blobs are non-copyable.
 
@@ -69,41 +51,42 @@ class Blob final {
      * @param size                      Size of the subblob.
      * @return                          Subblob that shares the ownership of the underlying memory with this blob.
      */
-    Blob subBlob(size_t offset, size_t size = -1);
+    [[nodiscard]] Blob subBlob(size_t offset, size_t size = -1) const;
 
     /**
      * @param data                      Pointer to a `malloc`-allocated memory region.
      * @param size                      Size of the memory region.
      * @return                          Blob that takes ownership of the provided memory region.
      */
-    static Blob fromMalloc(const void *data, size_t size);
+    [[nodiscard]] static Blob fromMalloc(const void *data, size_t size);
+    [[nodiscard]] static Blob fromMalloc(std::unique_ptr<void, FreeDeleter> data, size_t size);
 
     /**
      * @param path                      Path to a file.
      * @return                          Blob that wraps the memory mapping of the provided file.
      * @throws std::runtime_error       On error.
      */
-    static Blob fromFile(std::string_view path);
+    [[nodiscard]] static Blob fromFile(std::string_view path);
 
     /**
      * @param string                    String to create a blob from.
      * @return                          Blob that wraps the provided string.
      */
-    static Blob fromString(std::string string);
+    [[nodiscard]] static Blob fromString(std::string string);
 
     /**
      * @param data                      Memory region pointer.
      * @param size                      Memory region size.
      * @return                          Blob that owns a copy of the provided memory region.
      */
-    static Blob copy(const void *data, size_t size); // NOLINT: this is not std::copy
+    [[nodiscard]] static Blob copy(const void *data, size_t size); // NOLINT: this is not std::copy
 
     /**
      * @param data                      Memory region pointer.
      * @param size                      Memory region size.
      * @return                          Non-owning blob view into the provided memory region.
      */
-    static Blob view(const void *data, size_t size);
+    [[nodiscard]] static Blob view(const void *data, size_t size);
 
     /**
      * @param file                      File to read from.
@@ -111,7 +94,7 @@ class Blob final {
      * @return                          Blob that owns the data that was read from the provided file.
      * @throws Exception                If the provided number of bytes couldn't be read.
      */
-    static Blob read(FILE *file, size_t size);
+    [[nodiscard]] static Blob read(FILE *file, size_t size);
 
     /**
      * @param file                      File to read from.
@@ -119,7 +102,7 @@ class Blob final {
      * @return                          Blob that owns the data that was read from the provided file.
      * @throws Exception                If the provided number of bytes couldn't be read.
      */
-    static Blob read(FileInputStream &file, size_t size);
+    [[nodiscard]] static Blob read(FileInputStream &file, size_t size);
 
     /**
      * @param l                         First blob.
@@ -127,44 +110,47 @@ class Blob final {
      * @return                          Newly allocated blob that contains a concatenation of the data of the two
      *                                  provided blobs.
      */
-    static Blob concat(const Blob &l, const Blob &r);
+    [[nodiscard]] static Blob concat(const Blob &l, const Blob &r);
+
+    /**
+     * @param other                     Blob to share.
+     * @return                          Blob that shares the memory ownership with `other`.
+     */
+    [[nodiscard]] static Blob share(const Blob &other);
 
     friend void swap(Blob &l, Blob &r) {
-        std::swap(l._data, r._data);
-        std::swap(l._size, r._size);
-        std::swap(l._handler, r._handler);
+        using std::swap;
+        swap(l._data, r._data);
+        swap(l._size, r._size);
+        swap(l._state, r._state);
     }
 
-    size_t size() const {
+    [[nodiscard]] size_t size() const {
         return _size;
     }
 
-    bool empty() const {
+    [[nodiscard]] bool empty() const {
         return _size == 0;
     }
 
-    const void *data() const {
+    [[nodiscard]] const void *data() const {
         return _data;
     }
 
-    bool operator!() const {
+    [[nodiscard]] bool operator!() const {
         return empty();
     }
 
-    explicit operator bool() const {
+    [[nodiscard]] explicit operator bool() const {
         return !empty();
     }
 
-    std::string_view string_view() const {
-        return std::string_view(static_cast<const char *>(_data), _size);
-    }
-
-    BlobHandler *handler() const {
-        return _handler;
+    [[nodiscard]] std::string_view string_view() const {
+        return {static_cast<const char *>(_data), _size};
     }
 
  private:
     const void *_data = nullptr;
     size_t _size = 0;
-    BlobHandler *_handler = nullptr;
+    std::shared_ptr<void> _state;
 };
