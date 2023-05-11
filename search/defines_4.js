1fc7909019bb1e7b46e0e378966e108f909b2cea
captainurist <73941350+captainurist@users.noreply.github.com>
2023-05-11 10:07:55 +0000

    Deploying to gh-pages from @ OpenEnroth/OpenEnroth@be18c6612ce1dbc0255847eb7c2d0a1728bef028 🚀

--- search/defines_4.js
+++ search/defines_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['day_5fattrib_5ffog_0',['DAY_ATTRIB_FOG',['../Outdoor_8h.html#a8f7e5851c250151ad4eaca18a88e1d86',1,'Outdoor.h']]],
+  ['day_5fattrib_5ffog_0',['DAY_ATTRIB_FOG',['../LocationEnums_8h.html#a8f7e5851c250151ad4eaca18a88e1d86',1,'LocationEnums.h']]],
   ['deck_5fsize_1',['DECK_SIZE',['../Arcomage_8h.html#ab48a679225d82ec51e09e62d14313b34',1,'Arcomage.h']]]
 ];
--- search/enums_a.js
+++ search/enums_a.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['lastindex_0',['LastIndex',['../namespacedetail.html#ad7e74ca17ca07607d4d237aa8149040a',1,'detail']]],
-  ['level_5ftype_1',['LEVEL_TYPE',['../Indoor_8h.html#a24bf8802234f411139f3a641894c3f70',1,'Indoor.h']]],
+  ['level_5ftype_1',['LEVEL_TYPE',['../LocationEnums_8h.html#a24bf8802234f411139f3a641894c3f70',1,'LocationEnums.h']]],
   ['leveldecorationflag_2',['LevelDecorationFlag',['../Decoration_8h.html#a17eaffe474f750b97348969dfc31b138',1,'Decoration.h']]],
   ['locationflag_3',['LocationFlag',['../DecalBuilder_8h.html#af07e5b4c09b3a5482d69151f92f4cb94',1,'DecalBuilder.h']]],
   ['lodversion_4',['LodVersion',['../LodVersion_8h.html#a63d4571b1e87db9efa0a36b67d764103',1,'LodVersion.h']]],
