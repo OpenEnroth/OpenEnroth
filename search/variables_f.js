10644b0bd5e4a1569414bf685d2dd0e8527609cd
pskelton <pskelton@users.noreply.github.com>
2023-05-12 10:30:46 +0000

    Deploying to gh-pages from @ OpenEnroth/OpenEnroth@9d041bc55e67445aecf629ae54c651507f2f88bc 🚀

--- search/variables_f.js
+++ search/variables_f.js
@@ -37,8 +37,8 @@ var searchData=
   ['outdoor_5fno_5fwavy_5fwater_34',['outdoor_no_wavy_water',['../structODMRenderParams.html#a6827cf3b9a8b9bd0c1fad130b639280e',1,'ODMRenderParams']]],
   ['outputpresent_35',['outputPresent',['../RenderOpenGL_8cpp.html#afff3b3efd76e25649c87a86515ba4508',1,'RenderOpenGL.cpp']]],
   ['outputrender_36',['outputRender',['../RenderOpenGL_8cpp.html#af1f1163a9c8376b96a86751db3256db9',1,'RenderOpenGL.cpp']]],
-  ['overlayid_37',['overlayID',['../structSpellBuff.html#a71246715e120b97bffc46a7f6941ce6e',1,'SpellBuff']]],
-  ['overlayid_38',['overlayId',['../structSpellBuff__MM7.html#a0b20ebfff0491f10f19c2e4769ccca66',1,'SpellBuff_MM7']]],
+  ['overlayid_37',['overlayId',['../structSpellBuff__MM7.html#a0b20ebfff0491f10f19c2e4769ccca66',1,'SpellBuff_MM7']]],
+  ['overlayid_38',['overlayID',['../structSpellBuff.html#a71246715e120b97bffc46a7f6941ce6e',1,'SpellBuff']]],
   ['overlays_39',['overlays',['../structActiveOverlayList__MM7.html#a0adcd441f7eec0513d8d911cd19059d8',1,'ActiveOverlayList_MM7::overlays()'],['../structSaveGame__MM7.html#a7554f18d1fb01cec9dfdb1c2f37db998',1,'SaveGame_MM7::overlays()']]],
   ['ownitems_40',['ownItems',['../structPlayer__MM7.html#ae9d0dd7b371e0cc07448636882f11b92',1,'Player_MM7']]]
 ];
