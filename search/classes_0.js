e494eddd06b8d4659cf716132bc65ab4786e2fab
pskelton <pskelton@users.noreply.github.com>
2023-05-18 21:52:34 +0000

    Deploying to gh-pages from @ OpenEnroth/OpenEnroth@708700b8140775f1660c468a1a8b989e7c86c351 🚀

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['_5fevt_5fraw_0',['_evt_raw',['../struct__evt__raw.html',1,'']]],
-  ['_5frgbcolor_1',['_RGBColor',['../struct__RGBColor.html',1,'']]]
+  ['_5fevt_5fraw_0',['_evt_raw',['../struct__evt__raw.html',1,'']]]
 ];
--- search/typedefs_d.js
+++ search/typedefs_d.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['recti_0',['Recti',['../Rect_8h.html#a32073cc1967b01302be5d39e09079649',1,'Rect.h']]],
-  ['reference_1',['reference',['../classdetail_1_1RangeIterator.html#ae8c8fbfa4ba36b2ce4382fa136b792db',1,'detail::RangeIterator::reference()'],['../classSegment.html#af0ff246cdf8ecd8984307446636ea09c',1,'Segment::reference()']]],
-  ['rgbcolor_2',['RGBColor',['../GUIWindow_8cpp.html#acbc9da7672f18f747dcc88210702a6e9',1,'GUIWindow.cpp']]]
+  ['reference_1',['reference',['../classdetail_1_1RangeIterator.html#ae8c8fbfa4ba36b2ce4382fa136b792db',1,'detail::RangeIterator::reference()'],['../classSegment.html#af0ff246cdf8ecd8984307446636ea09c',1,'Segment::reference()']]]
 ];
