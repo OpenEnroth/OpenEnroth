98d7e2995dcb2ade121e40d00a825f84be2c92b7
captainurist <73941350+captainurist@users.noreply.github.com>
2023-05-24 19:13:58 +0800

    Added 2nd test for #735

--- test/Tests/TestIssues.cpp
+++ test/Tests/TestIssues.cpp
@@ -945,6 +945,11 @@ GAME_TEST(Issues, Issue735a) {
     test->playTraceFromTestData("issue_735a.mm7", "issue_735a.json");
 }
 
+GAME_TEST(Issues, Issue735b) {
+    // Trace-only test: battle with over 30 monsters in the open.
+    test->playTraceFromTestData("issue_735b.mm7", "issue_735b.json");
+}
+
 GAME_TEST(Issues, Issue741) {
     // Game crashing when walking into a wall in Temple of the moon
     test->playTraceFromTestData("issue_741.mm7", "issue_741.json");
