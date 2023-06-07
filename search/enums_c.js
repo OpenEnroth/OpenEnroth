b0b4fc54c9b289d795be40a47c81315f72100c13
captainurist <73941350+captainurist@users.noreply.github.com>
2023-06-07 11:21:52 +0000

    Deploying to gh-pages from @ OpenEnroth/OpenEnroth@549ef4b100e90d04bbe9faf197a300b30156aa9d 🚀

--- search/enums_c.js
+++ search/enums_c.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['npcprof_0',['NPCProf',['../NPCEnums_8h.html#ae6501f571f97d74c2c78fddcb371bad4',1,'NPCEnums.h']]],
-  ['nuklear_5faction_1',['NUKLEAR_ACTION',['../classNuklear.html#a267bf08292d09792b96bfabe87bc4b1c',1,'Nuklear']]],
-  ['nuklear_5fmode_2',['NUKLEAR_MODE',['../classNuklear.html#a54e0d3f4afb9724a4744dbf8b56b4bf0',1,'Nuklear']]],
-  ['nuklear_5fstage_3',['NUKLEAR_STAGE',['../classNuklear.html#a7254a0bd933c66f285a875fbfa0129fb',1,'Nuklear']]]
+  ['npcflag_0',['NpcFlag',['../NPCTable_8h.html#aac94c9aaebe985e90dba084e900a99a3',1,'NPCTable.h']]],
+  ['npcprof_1',['NPCProf',['../NPCEnums_8h.html#ae6501f571f97d74c2c78fddcb371bad4',1,'NPCEnums.h']]],
+  ['nuklear_5faction_2',['NUKLEAR_ACTION',['../classNuklear.html#a267bf08292d09792b96bfabe87bc4b1c',1,'Nuklear']]],
+  ['nuklear_5fmode_3',['NUKLEAR_MODE',['../classNuklear.html#a54e0d3f4afb9724a4744dbf8b56b4bf0',1,'Nuklear']]],
+  ['nuklear_5fstage_4',['NUKLEAR_STAGE',['../classNuklear.html#a7254a0bd933c66f285a875fbfa0129fb',1,'Nuklear']]]
 ];
