91f2a76b5653a27bc85b453b2cd1af2fc2fc5dcd
captainurist <73941350+captainurist@users.noreply.github.com>
2023-06-18 20:18:05 +0800

    Fix some of the missing text shadows

--- src/GUI/GUIFont.cpp
+++ src/GUI/GUIFont.cpp
@@ -135,6 +135,8 @@ int GUIFont::GetHeight() const {
 }
 
 void GUIFont::DrawTextLine(const std::string &text, Color color, Pointi position, int max_len_pix) {
+    // TODO(captainurist): why is color parameter unused?
+
     if (text.empty()) {
         return;
     }
@@ -181,7 +183,7 @@ void GUIFont::DrawTextLine(const std::string &text, Color color, Pointi position
                     float v1 = (ysq * 32.0f) / 512.0f;
                     float v2 = (ysq * 32.0f + pData->uFontHeight) / 512.0f;
 
-                    render->DrawTextNew(uX_pos, position.y, pData->pMetrics[c].uWidth, pData->uFontHeight, u1, v1, u2, v2, 1, Color());
+                    render->DrawTextNew(uX_pos, position.y, pData->pMetrics[c].uWidth, pData->uFontHeight, u1, v1, u2, v2, 1, colorTable.Black);
                     render->DrawTextNew(uX_pos, position.y, pData->pMetrics[c].uWidth, pData->uFontHeight, u1, v1, u2, v2, 0, draw_color);
 
                     uX_pos += uCharWidth;
@@ -690,7 +692,7 @@ int GUIFont::DrawTextInRect(GUIWindow *window, Pointi position, Color color, con
                     float v1 = (ysq * 32.0f) / 512.0f;
                     float v2 = (ysq * 32.0f + pData->uFontHeight) / 512.0f;
 
-                    render->DrawTextNew(text_pos_x, text_pos_y, pData->pMetrics[v15].uWidth, pData->uFontHeight, u1, v1, u2, v2, 1, Color());
+                    render->DrawTextNew(text_pos_x, text_pos_y, pData->pMetrics[v15].uWidth, pData->uFontHeight, u1, v1, u2, v2, 1, colorTable.Black);
                     render->DrawTextNew(text_pos_x, text_pos_y, pData->pMetrics[v15].uWidth, pData->uFontHeight, u1, v1, u2, v2, 0, color);
                 } else {
                     // pallette24 check for colour
@@ -717,7 +719,7 @@ int GUIFont::DrawTextInRect(GUIWindow *window, Pointi position, Color color, con
                     float v2 = (ysq * 32.0f + pData->uFontHeight) / 512.0f;
 
                     render->DrawTextNew(text_pos_x, text_pos_y, pData->pMetrics[v15].uWidth, pData->uFontHeight, u1, v1, u2, v2, 0, Color(r, g, b));
-                    render->DrawTextNew(text_pos_x, text_pos_y, pData->pMetrics[v15].uWidth, pData->uFontHeight, u1, v1, u2, v2, 1, Color());
+                    render->DrawTextNew(text_pos_x, text_pos_y, pData->pMetrics[v15].uWidth, pData->uFontHeight, u1, v1, u2, v2, 1, colorTable.Black);
                 }
                 text_pos_x += char_width;
                 if (i < (int)pNumLen) {
