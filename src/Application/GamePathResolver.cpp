21f5b12aa1ac503a0a1d78cfe4f7b14ee7e630b8
botanicvelious <cyberempires@gmail.com>
2023-07-01 11:34:50 -0600

    Update GamePathResolver.cpp

--- src/Application/GamePathResolver.cpp
+++ src/Application/GamePathResolver.cpp
@@ -55,7 +55,7 @@ static std::string _resolvePath(
     // TODO: find a better way to deal with paths and remove this android specific block.
     std::string result = platform->storagePath(ANDROID_STORAGE_EXTERNAL);
     if (result.empty())
-        result = platform->StoragePath(ANDROID_STORAGE_INTERNAL);
+        result = platform->storagePath(ANDROID_STORAGE_INTERNAL);
     if (result.empty())
         platform->ShowMessageBox("Device currently unsupported", "Your device doesn't have any storage so it is unsupported!");
     return result;
