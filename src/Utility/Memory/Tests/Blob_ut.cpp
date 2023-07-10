7efacb3f822942525c5051b265f3205bfb78190f
captainurist <73941350+captainurist@users.noreply.github.com>
2023-07-10 10:03:29 +0800

    Test for Blob::fromFile

---
+++ src/Utility/Memory/Tests/Blob_ut.cpp
@@ -0,0 +1,29 @@
+#include <filesystem>
+
+#include "Testing/Unit/UnitTest.h"
+
+#include "Utility/Memory/Blob.h"
+#include "Utility/Streams/FileOutputStream.h"
+
+UNIT_TEST(Blob, FromFile) {
+    std::string fileName = "abcdefghijklmnopqrstuvwxyz.tmp";
+    std::string fileContents = "abcd";
+
+    auto cleanup = [&] {
+        std::error_code ec;
+        std::filesystem::remove(fileName, ec);
+    };
+    cleanup(); // Just in case.
+
+    Blob blob;
+    EXPECT_THROW(blob = Blob::fromFile(fileName), std::runtime_error);
+
+    FileOutputStream output(fileName);
+    output.write(fileContents);
+    output.close();
+
+    blob = Blob::fromFile(fileName);
+    EXPECT_EQ(blob.string_view(), fileContents);
+
+    cleanup();
+}
