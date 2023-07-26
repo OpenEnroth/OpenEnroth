8e72120556319240ff715fd6b5e8c759275c9b64
captainurist <73941350+captainurist@users.noreply.github.com>
2023-07-26 10:04:54 +0800

    Style for enum class RendererType & added RENDERER_NULL

--- src/Engine/Graphics/OpenGL/RenderOpenGL.cpp
+++ src/Engine/Graphics/OpenGL/RenderOpenGL.cpp
@@ -4593,7 +4593,7 @@ bool RenderOpenGL::Initialize() {
         PlatformOpenGLOptions opts;
 
         // Set it only on startup as currently we don't support multiple contexts to be able to switch OpenGL<->OpenGLES in the middle of runtime.
-        OpenGLES = config->graphics.Renderer.value() == RendererType::OpenGLES;
+        OpenGLES = config->graphics.Renderer.value() == RENDERER_OPENGL_ES;
 
         if (!OpenGLES) {
             //  Use OpenGL 4.1 core
