d26ccbd6fd4352c3a56efdb265427f0f5f12454d
captainurist <73941350+captainurist@users.noreply.github.com>
2023-07-28 13:34:39 +0800

    Do not indent namespaces

--- src/Io/IKeyboardController.h
+++ src/Io/IKeyboardController.h
@@ -4,10 +4,10 @@
 
 namespace Io {
 
-    // Glue interface between Game and Platform-specific controller
-    class IKeyboardController {
-     public:
-        virtual bool ConsumeKeyPress(PlatformKey key) = 0;
-        virtual bool IsKeyDown(PlatformKey key) const = 0;
-    };
-}
+// Glue interface between Game and Platform-specific controller
+class IKeyboardController {
+ public:
+    virtual bool ConsumeKeyPress(PlatformKey key) = 0;
+    virtual bool IsKeyDown(PlatformKey key) const = 0;
+};
+} // namespace Io
--- src/Io/KeyboardInputHandler.h
+++ src/Io/KeyboardInputHandler.h
@@ -24,57 +24,57 @@ using enum WindowInputStatus;
 namespace Io {
     // Handles events from OSWindow through GameWindowHandler/IKeyboardController
     //      and maps it to game actions/events using KeyboardActionMapping
-    class KeyboardInputHandler {
-     public:
-        KeyboardInputHandler(IKeyboardController *controller, std::shared_ptr<KeyboardActionMapping> actionMapping) {
-            this->controller = controller;
-            this->actionMapping = actionMapping;
+class KeyboardInputHandler {
+ public:
+    KeyboardInputHandler(IKeyboardController *controller, std::shared_ptr<KeyboardActionMapping> actionMapping) {
+        this->controller = controller;
+        this->actionMapping = actionMapping;
 
-            lastKeyPressed = PlatformKey::KEY_NONE;
-            inputType = TextInputType::None;
-            window = nullptr;
-            ResetKeys();
-        }
+        lastKeyPressed = PlatformKey::KEY_NONE;
+        inputType = TextInputType::None;
+        window = nullptr;
+        ResetKeys();
+    }
 
-        bool IsKeyboardPickingOutlineToggled() const;
-        bool IsRunKeyToggled() const;
-        bool IsTurnStrafingToggled() const;
-        bool IsStealingToggled() const;
-        bool IsTakeAllToggled() const;
-        bool IsAdventurerBackcycleToggled() const;
-        bool IsSpellBackcycleToggled() const;
-        bool IsCastOnClickToggled() const;
-        bool IsKeyHeld(PlatformKey key) const;
+    bool IsKeyboardPickingOutlineToggled() const;
+    bool IsRunKeyToggled() const;
+    bool IsTurnStrafingToggled() const;
+    bool IsStealingToggled() const;
+    bool IsTakeAllToggled() const;
+    bool IsAdventurerBackcycleToggled() const;
+    bool IsSpellBackcycleToggled() const;
+    bool IsCastOnClickToggled() const;
+    bool IsKeyHeld(PlatformKey key) const;
 
-        void GenerateInputActions();
+    void GenerateInputActions();
 
-        void SetWindowInputStatus(WindowInputStatus status);
-        inline PlatformKey LastPressedKey() const {
-            return lastKeyPressed;
-        }
-        void ResetKeys();
+    void SetWindowInputStatus(WindowInputStatus status);
+    inline PlatformKey LastPressedKey() const {
+        return lastKeyPressed;
+    }
+    void ResetKeys();
 
-        void StartTextInput(TextInputType type, int max_string_len, GUIWindow *pWindow);
-        bool ProcessTextInput(PlatformKey key, int c);
-        void EndTextInput();
+    void StartTextInput(TextInputType type, int max_string_len, GUIWindow *pWindow);
+    bool ProcessTextInput(PlatformKey key, int c);
+    void EndTextInput();
 
-        const std::string &GetTextInput() const;
-        void SetTextInput(const std::string &text);
+    const std::string &GetTextInput() const;
+    void SetTextInput(const std::string &text);
 
-     private:
-        void GeneratePausedActions();
-        void GenerateGameplayActions();
+ private:
+    void GeneratePausedActions();
+    void GenerateGameplayActions();
 
-        IKeyboardController *controller;
-        std::shared_ptr<KeyboardActionMapping> actionMapping;
+    IKeyboardController *controller;
+    std::shared_ptr<KeyboardActionMapping> actionMapping;
 
-        PlatformKey lastKeyPressed;
-        int keydelaytimer;
-        int max_input_string_len;
-        std::string pPressedKeysBuffer;
-        TextInputType inputType;
-        GUIWindow *window;
-    };
+    PlatformKey lastKeyPressed;
+    int keydelaytimer;
+    int max_input_string_len;
+    std::string pPressedKeysBuffer;
+    TextInputType inputType;
+    GUIWindow *window;
+};
 }  // namespace Io
 
 
