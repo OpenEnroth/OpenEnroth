5dfc98f7bba998b7744dabeec1ae6bd0ff9d5746
captainurist <73941350+captainurist@users.noreply.github.com>
2023-07-29 09:21:39 +0000

    Deploying to gh-pages from @ OpenEnroth/OpenEnroth@df17da0f7f95feeddd0cad8129f383976d1fd86e 🚀

--- search/defines_f.js
+++ search/defines_f.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['warn_0',['Warn',['../ErrorHandling_8h.html#a0e8095ce1ed00bd4150a20c72d4e4330',1,'ErrorHandling.h']]],
-  ['win32_5flean_5fand_5fmean_1',['WIN32_LEAN_AND_MEAN',['../WinPlatform_8cpp.html#ac7bef5d85e3dcd73eef56ad39ffc84a9',1,'WinPlatform.cpp']]]
+  ['win32_5flean_5fand_5fmean_0',['WIN32_LEAN_AND_MEAN',['../WinPlatform_8cpp.html#ac7bef5d85e3dcd73eef56ad39ffc84a9',1,'WinPlatform.cpp']]]
 ];
--- search/typedefs_3.js
+++ search/typedefs_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['enumeration_5ftype_0',['enumeration_type',['../classFlags.html#a9d5d5877e991d0c2a331568dac527f54',1,'Flags']]]
+  ['enumeration_5ftype_0',['enumeration_type',['../classFlags.html#a9d5d5877e991d0c2a331568dac527f54',1,'Flags']]],
+  ['errorhandlerfunction_1',['ErrorHandlerFunction',['../ErrorHandling_8h.html#a10c8cb7454f6f59efb4f3c564ed5e4ee',1,'ErrorHandling.h']]]
 ];
