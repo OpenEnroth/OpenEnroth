d84152ee95a2ab4e21c8a71693eeffb282b255c2
captainurist <73941350+captainurist@users.noreply.github.com>
2023-08-07 18:48:18 +0800

    Fuzzy math funcs take eps as a parameter

--- src/Utility/Math/Float.h
+++ src/Utility/Math/Float.h
@@ -3,18 +3,18 @@
 #include <cmath>
 #include <algorithm>
 
-[[nodiscard]] inline bool fuzzyIsNull(float value) {
-    return std::abs(value) < 0.00001f;
+[[nodiscard]] inline bool fuzzyIsNull(float value, float eps = 0.00001f) {
+    return std::abs(value) < eps;
 }
 
-[[nodiscard]] inline bool fuzzyIsNull(double value) {
-    return std::abs(value) < 0.000000000001;
+[[nodiscard]] inline bool fuzzyIsNull(double value, double eps = 0.000000000001) {
+    return std::abs(value) < eps;
 }
 
-[[nodiscard]] inline bool fuzzyEquals(float l, float r) {
-    return std::abs(l - r) * 100000.f <= std::min(std::abs(l), std::abs(r));
+[[nodiscard]] inline bool fuzzyEquals(float l, float r, float eps = 0.00001f) {
+    return std::abs(l - r) <= std::min(std::abs(l), std::abs(r)) * eps;
 }
 
-[[nodiscard]] inline bool fuzzyEquals(double l, double r) {
-    return std::abs(l - r) * 1000000000000. <= std::min(std::abs(l), std::abs(r));
+[[nodiscard]] inline bool fuzzyEquals(double l, double r, double eps = 0.000000000001) {
+    return std::abs(l - r) <= std::min(std::abs(l), std::abs(r)) * eps;
 }
