3a734d31d9868340c70942eebb927a780bc1185b
captainurist <73941350+captainurist@users.noreply.github.com>
2023-08-08 08:25:49 +0000

    Deploying to gh-pages from @ OpenEnroth/OpenEnroth@5fc2d5f25660575f10f94e20dc68e1833f3b1a82 🚀

--- search/defines_1.js
+++ search/defines_1.js
@@ -3,6 +3,5 @@ var searchData=
   ['add_5fnpc_5fscripted_5fdialogue_0',['ADD_NPC_SCRIPTED_DIALOGUE',['../NPCTopics_8cpp.html#a2517cd77451937536e693a426e63e943',1,'NPCTopics.cpp']]],
   ['apply_5fto_5fboth_1',['APPLY_TO_BOTH',['../Arcomage_8cpp.html#ab4b6e488f7717cbb1ea499cdc1c3b9aa',1,'Arcomage.cpp']]],
   ['apply_5fto_5fenemy_2',['APPLY_TO_ENEMY',['../Arcomage_8cpp.html#aad66a61c05caeb30d5c7cad1c7228ae3',1,'Arcomage.cpp']]],
-  ['apply_5fto_5fplayer_3',['APPLY_TO_PLAYER',['../Arcomage_8cpp.html#a80b4e5803cab2c360d6c78cdf0bbf005',1,'Arcomage.cpp']]],
-  ['assert_4',['Assert',['../ErrorHandling_8h.html#a95eb55fc233c36dca688edf7ff1389d6',1,'ErrorHandling.h']]]
+  ['apply_5fto_5fplayer_3',['APPLY_TO_PLAYER',['../Arcomage_8cpp.html#a80b4e5803cab2c360d6c78cdf0bbf005',1,'Arcomage.cpp']]]
 ];
