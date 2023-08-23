287fde432fc31c836e5a1f462ed1018113abb52b
Alexander Izmailov <yarolig@gmail.com>
2023-08-23 14:23:01 +0300

    fix assert when right clicking on hireling with no active character

--- src/GUI/UI/UIPopup.cpp
+++ src/GUI/UI/UIPopup.cpp
@@ -1660,7 +1660,7 @@ void GameUI_DrawNPCPopup(void *_this) {  // PopupWindowForBenefitAndJoinText
                 popup_window.DrawTitleText(assets->pFontArrus.get(), 0, 12, colorTable.PaleCanary, NameAndTitle(pNPC), 3);
                 popup_window.uFrameWidth -= 24;
                 popup_window.uFrameZ = popup_window.uFrameX + popup_window.uFrameWidth - 1;
-                popup_window.DrawText(assets->pFontArrus.get(), {100, 36}, colorTable.White, BuildDialogueString(pText, pParty->activeCharacterIndex() - 1, 0, HOUSE_INVALID, 0));
+                popup_window.DrawText(assets->pFontArrus.get(), {100, 36}, colorTable.White, BuildDialogueString(pText, 0, 0, HOUSE_INVALID, 0));
             }
         }
     }
