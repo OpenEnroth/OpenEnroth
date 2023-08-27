884ab8def04b33be399c64e5f27f9d0026b879db
Peter Skelton <peter.r.skelton@gmail.com>
2023-08-27 09:37:26 +0100

    Fix and test #1255

--- test/Bin/GameTest/GameTests.cpp
+++ test/Bin/GameTest/GameTests.cpp
@@ -1642,6 +1642,13 @@ GAME_TEST(Issues, Issue1251) {
     EXPECT_EQ(charmedactors.delta(), 3);
 }
 
+GAME_TEST(Issues, Issue1255) {
+    // Cant buy green wand
+    EXPECT_FALSE(pParty->hasItem(ITEM_FAIRY_WAND_OF_LASHING));
+    test.playTraceFromTestData("issue_1255.mm7", "issue_1255.json");
+    EXPECT_TRUE(pParty->hasItem(ITEM_FAIRY_WAND_OF_LASHING));
+}
+
 GAME_TEST(Issues, Issue1273) {
     // Assert when clicking on shop video area
     auto dialogueTape = tapes.dialogueType();
