ffe75189c9ed91cd35724fa3fb1c17f41773bc12
captainurist <73941350+captainurist@users.noreply.github.com>
2023-09-17 09:06:42 +0800

    Docs

--- src/Engine/Events/EventMap.cpp
+++ src/Engine/Events/EventMap.cpp
@@ -1,7 +1,9 @@
+#include "EventMap.h"
+
 #include <vector>
+#include <utility>
 
 #include "Engine/Party.h"
-#include "Engine/Events/EventMap.h"
 #include "Engine/Engine.h"
 #include "Engine/EngineIocContainer.h"
 
@@ -13,15 +15,37 @@
 
 #include "RawEvent.h"
 
+EventMap EventMap::load(const Blob &rawData) {
+    EventMap result;
+
+    const char *pos = reinterpret_cast<const char *>(rawData.data());
+    const char *end = pos + rawData.size();
+    while (pos < end) {
+        const RawEvent *evt = reinterpret_cast<const RawEvent *>(pos);
+
+        size_t size = evt->_e_size + 1; // +1 because we also count the size byte.
+        if (size < 5)
+            throw Exception("Invalid evt record size: expected at least {}, got {}", 5, size);
+        if (pos + size > end)
+            throw Exception("Encountered corrupted evt binary data");
+
+        int eventId = EVT_WORD(&evt->v1);
+        result.add(eventId, EventIR::parse(evt, size));
+        pos += size;
+    }
+
+    return result;
+}
+
 void EventMap::add(int eventId, EventIR ir) {
-    _eventsById[eventId].push_back(ir);
+    _eventsById[eventId].push_back(std::move(ir));
 }
 
 void EventMap::clear() {
     _eventsById.clear();
 }
 
-EventIR EventMap::event(int eventId, int step) const {
+const EventIR &EventMap::event(int eventId, int step) const {
     for (const EventIR &ir : events(eventId))
         if (ir.step == step)
             return ir;
@@ -54,25 +78,23 @@ std::vector<EventTrigger> EventMap::enumerateTriggers(EventType triggerType) {
 }
 
 bool EventMap::hasHint(int eventId) const {
-    if (!_eventsById.contains(eventId))
+    const auto* events = valuePtr(_eventsById, eventId);
+    if (!events || events->size() < 2)
         return false;
 
-    const std::vector<EventIR>& events = _eventsById.at(eventId);
-    if (events.size() < 2)
-        return false;
-
-    return events[0].type == EVENT_MouseOver && events[1].type == EVENT_Exit;
+    return (*events)[0].type == EVENT_MouseOver && (*events)[1].type == EVENT_Exit;
 }
 
 std::string EventMap::hint(int eventId) const {
     std::string result;
     bool mouseOverFound = false;
 
-    if (!_eventsById.contains(eventId)) { // no entry in .evt file
+    const auto* events = valuePtr(_eventsById, eventId);
+    if (!events) { // no entry in .evt file
         return result;
     }
 
-    for (const EventIR &ir : _eventsById.at(eventId)) {
+    for (const EventIR &ir : *events) {
         if (ir.type == EVENT_MouseOver) {
             mouseOverFound = true;
             if (ir.data.text_id < engine->_levelStrings.size()) {
@@ -80,7 +102,7 @@ std::string EventMap::hint(int eventId) const {
             }
         }
         if (mouseOverFound && ir.type == EVENT_SpeakInHouse) {
-            if (allHouses().contains(ir.data.house_id)) {
+            if (buildingTable.indices().contains(ir.data.house_id)) {
                 result = buildingTable[ir.data.house_id].pName;
             }
             break;
@@ -91,9 +113,10 @@ std::string EventMap::hint(int eventId) const {
 }
 
 void EventMap::dump(int eventId) const {
-    if (_eventsById.contains(eventId)) {
+    const auto *events = valuePtr(_eventsById, eventId);
+    if (events) {
         logger->verbose("Event: {}", eventId);
-        for (const EventIR &ir : _eventsById.at(eventId)) {
+        for (const EventIR &ir : *events) {
             logger->verbose("{}", ir.toString());
         }
     } else {
@@ -105,26 +128,4 @@ void EventMap::dumpAll() const {
     for (const auto &[id, _] : _eventsById) {
         dump(id);
     }
-}
-
-EventMap EventMap::load(const Blob &rawData) {
-    EventMap result;
-
-    const char *pos = reinterpret_cast<const char *>(rawData.data());
-    const char *end = pos + rawData.size();
-    while (pos < end) {
-        const RawEvent *evt = reinterpret_cast<const RawEvent *>(pos);
-
-        size_t size = evt->_e_size + 1; // +1 because we also count the size byte.
-        if (size < 5)
-            throw Exception("Invalid evt record size: expected at least {}, got {}", 5, size);
-        if (pos + size > end)
-            throw Exception("Encountered corrupted evt binary data");
-
-        int eventId = EVT_WORD(&evt->v1);
-        result.add(eventId, EventIR::parse(evt, size));
-        pos += size;
-    }
-
-    return result;
 }
