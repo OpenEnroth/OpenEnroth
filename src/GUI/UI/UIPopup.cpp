154f79b6e1ec578eacd5782ea0c9a5aec3b63ae8
captainurist <73941350+captainurist@users.noreply.github.com>
2023-09-18 16:22:35 +0800

    Default arguments for BuildDialogueString

--- src/GUI/UI/UIPopup.cpp
+++ src/GUI/UI/UIPopup.cpp
@@ -1671,7 +1671,7 @@ void GameUI_DrawNPCPopup(void *_this) {  // PopupWindowForBenefitAndJoinText
                 popup_window.DrawTitleText(assets->pFontArrus.get(), 0, 12, colorTable.PaleCanary, NameAndTitle(pNPC), 3);
                 popup_window.uFrameWidth -= 24;
                 popup_window.uFrameZ = popup_window.uFrameX + popup_window.uFrameWidth - 1;
-                popup_window.DrawText(assets->pFontArrus.get(), {100, 36}, colorTable.White, BuildDialogueString(pText, 0, 0, HOUSE_INVALID, SHOP_SCREEN_INVALID));
+                popup_window.DrawText(assets->pFontArrus.get(), {100, 36}, colorTable.White, BuildDialogueString(pText, 0));
             }
         }
     }
