10b886a57aeae675831e66275386d1170dfe2eca
captainurist <73941350+captainurist@users.noreply.github.com>
2023-09-19 11:22:01 +0800

    Move platform_main into an OBJECT lib

--- src/Platform/Sdl/SdlMain.cpp
+++ src/Platform/Sdl/SdlMain.cpp
@@ -2,8 +2,7 @@
 
 int platformMain(int argc, char **argv);
 
-// This gets #defined by SDL into SDL_main. Marking it as extern "C" is important for name mangling as on Android
-// it gets exported from the resulting .so.
-extern "C" int main(int argc, char **argv) {
+// This gets #defined by SDL into SDL_main.
+int main(int argc, char **argv) {
     return platformMain(argc, argv);
 }
