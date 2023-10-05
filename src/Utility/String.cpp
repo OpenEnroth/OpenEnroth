f27307868d01441afb7b96b8308e53c8738cee6e
captainurist <73941350+captainurist@users.noreply.github.com>
2023-10-05 13:13:14 +0900

    replaceAll added

--- src/Utility/String.cpp
+++ src/Utility/String.cpp
@@ -1,6 +1,6 @@
 #include "String.h"
 
-#include <cstdarg>
+#include <cassert>
 #include <vector>
 #include <algorithm>
 
@@ -100,5 +100,23 @@ std::string toHexDump(std::string_view s, size_t groupSize) {
             result += " ";
         result += fmt::format("{:02X}", static_cast<uint8_t>(s[i]));
     }
+    return result;
+}
+
+std::string replaceAll(std::string_view text, std::string_view what, std::string_view replacement) {
+    assert(!what.empty());
+    std::string result;
+
+    size_t pos = 0;
+    while(true) {
+        size_t next = text.find(what, pos);
+        result.append(text.substr(pos, next == std::string_view::npos ? std::string_view::npos : next - pos));
+        if (next == std::string_view::npos)
+            break;
+
+        result.append(replacement);
+        pos = next + what.size();
+    }
+
     return result;
 }
