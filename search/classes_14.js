88b2ef74cf6da051accef6983004a75107fbc79b
captainurist <73941350+captainurist@users.noreply.github.com>
2023-11-07 23:08:39 +0000

    Deploying to gh-pages from @ OpenEnroth/OpenEnroth@3e59f3f592400e47095da7fd60a6a5f0ab5c3595 🚀

--- search/classes_14.js
+++ search/classes_14.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['weather_0',['Weather',['../classWeather.html',1,'']]],
   ['window_1',['Window',['../classGameConfig_1_1Window.html',1,'GameConfig']]],
-  ['windowmanager_2',['WindowManager',['../classWindowManager.html',1,'']]],
-  ['winenvironment_3',['WinEnvironment',['../classWinEnvironment.html',1,'']]]
+  ['windowdata_2',['WindowData',['../structWindowData.html',1,'']]],
+  ['windowmanager_3',['WindowManager',['../classWindowManager.html',1,'']]],
+  ['winenvironment_4',['WinEnvironment',['../classWinEnvironment.html',1,'']]]
 ];
--- search/enums_3.js
+++ search/enums_3.js
@@ -4,6 +4,5 @@ var searchData=
   ['decalflag_1',['DecalFlag',['../DecalBuilder_8h.html#ac489ae5fe7efc98274ba75ffb11f244d',1,'DecalBuilder.h']]],
   ['decorationdescflag_2',['DecorationDescFlag',['../DecorationList_8h.html#a0f02a4f1bd0fdf28d7f46f29f2d05310',1,'DecorationList.h']]],
   ['dialogue_5ftype_3',['DIALOGUE_TYPE',['../GUIDialogues_8h.html#afcd7885d9ec6583fa04d9e8f0f641d00',1,'GUIDialogues.h']]],
-  ['dialogwindowtype_4',['DialogWindowType',['../UIDialogue_8h.html#a7913c760eef3a9a5e8f39db31e1c1bc2',1,'UIDialogue.h']]],
-  ['doorattribute_5',['DoorAttribute',['../FaceEnums_8h.html#aca4fbb5b4731d33480ddc765e71c176e',1,'FaceEnums.h']]]
+  ['doorattribute_4',['DoorAttribute',['../FaceEnums_8h.html#aca4fbb5b4731d33480ddc765e71c176e',1,'FaceEnums.h']]]
 ];
