4866089f96d7572b4cf474f671542c9dadcc2b78
captainurist <73941350+captainurist@users.noreply.github.com>
2023-11-16 22:36:39 +0000

    Dropped enum SoundType

--- src/Engine/MM7.h
+++ src/Engine/MM7.h
@@ -6,11 +6,3 @@ typedef unsigned int uint;
 typedef unsigned char uchar;
 typedef unsigned short ushort;
 typedef unsigned long ulong;
-
-// TODO(captainurist): #enum class
-// TODO(captainurist): this duplicates enum SoundID.
-enum SoundType {
-    SOUND_EndTurnBasedMode = 0xCE,
-    SOUND_StartTurnBasedMode = 0xCF,
-    SOUND_FlipOnExit = 0x4E21,
-};
