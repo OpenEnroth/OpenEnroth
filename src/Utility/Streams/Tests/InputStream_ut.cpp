e0757b206920de0481427308bc7eaa78bd82c731
captainurist <73941350+captainurist@users.noreply.github.com>
2023-11-25 23:17:26 +0000

    Fix a bug in InputStream::readAll

---
+++ src/Utility/Streams/Tests/InputStream_ut.cpp
@@ -0,0 +1,11 @@
+#include "Testing/Unit/UnitTest.h"
+
+#include "Utility/Streams/MemoryInputStream.h"
+
+UNIT_TEST(InputStream, ReadAll) {
+    std::string largeString(10000, 'a');
+    MemoryInputStream input(largeString.data(), largeString.size());
+
+    std::string resultingString = input.readAll();
+    EXPECT_EQ(largeString, resultingString); // There was a bug in readAll resulting in failures on large files.
+}
