a45cd85e1c3ec74159118ebfd8524ee07d353513
captainurist <73941350+captainurist@users.noreply.github.com>
2023-11-26 23:55:06 +0000

    Moved LodFileFormat serialization where it belongs

---
+++ src/Library/LodFormats/LodFormatEnums.cpp
@@ -0,0 +1,12 @@
+#include "LodFormatEnums.h"
+
+#include "Library/Serialization/EnumSerialization.h"
+
+MM_DEFINE_ENUM_SERIALIZATION_FUNCTIONS(LodFileFormat, CASE_SENSITIVE, {
+    {LOD_FILE_COMPRESSED, "compressed_data"},
+    {LOD_FILE_IMAGE, "image"},
+    {LOD_FILE_PALETTE, "palette"},
+    {LOD_FILE_PSEUDO_IMAGE, "pseudo_image_data"},
+    {LOD_FILE_SPRITE, "sprite"},
+    {LOD_FILE_RAW, "raw_data"},
+})
