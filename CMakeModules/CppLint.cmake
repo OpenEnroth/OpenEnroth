a96f71dd0e24f6ed8f6ff78fd92371b6275451c9
captainurist <73941350+captainurist@users.noreply.github.com>
2023-12-03 14:33:56 +0000

    Added OE_CHECK_STYLE option

--- CMakeModules/CppLint.cmake
+++ CMakeModules/CppLint.cmake
@@ -1,43 +1,34 @@
-find_package(PythonInterp)
-
-set(CPPLINT_VERSION "1.6.1")
-
-if(PYTHONINTERP_FOUND)
-  set(CPPLINT_COMMAND "${PROJECT_SOURCE_DIR}/thirdparty/cpplint/cpplint.py" CACHE FILEPATH "CppLint command")
-  set(CPPLINT_FOUND ON CACHE BOOL "CppLint found")
-endif()
-
-function(enable_check_style)
-  if(CPPLINT_FOUND)
-    add_custom_target(check_style)
-  endif()
+function(init_check_style)
+    if(OE_CHECK_STYLE)
+        find_package(Python COMPONENTS Interpreter GLOBAL)
+        set(OE_CPPLINT_COMMAND "${PROJECT_SOURCE_DIR}/thirdparty/cpplint/cpplint.py" CACHE FILEPATH "CppLint command")
+        add_custom_target(check_style)
+    endif()
 endfunction()
 
 function(source_check_style TARGET TARGET_SOURCES)
-  if(CPPLINT_FOUND)
-    set(TARGET_NAME "check_style_${TARGET}")
+    if(OE_CHECK_STYLE)
+        set(TARGET_NAME "check_style_${TARGET}")
 
-    set(SOURCES_LIST)
-    foreach(SOURCE_FILE ${TARGET_SOURCES})
-        if(SOURCE_FILE MATCHES \\.c$|\\.cxx$|\\.cpp$|\\.cc$|\\.h$|\\.hh$)
-            list(APPEND SOURCES_LIST ${SOURCE_FILE})
-        endif()
-    endforeach(SOURCE_FILE)
+        set(SOURCES_LIST)
+        foreach(SOURCE_FILE ${TARGET_SOURCES})
+            if(SOURCE_FILE MATCHES \\.c$|\\.cxx$|\\.cpp$|\\.cc$|\\.h$|\\.hh$)
+                list(APPEND SOURCES_LIST ${SOURCE_FILE})
+            endif()
+        endforeach(SOURCE_FILE)
 
-    add_custom_target(${TARGET_NAME} ${PYTHON_EXECUTABLE} ${CPPLINT_COMMAND} "--quiet" ${SOURCES_LIST}
-                      DEPENDS ${SOURCES_LIST}
-                      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-    add_dependencies(check_style ${TARGET_NAME})
-    set_property(TARGET ${TARGET_NAME} PROPERTY FOLDER "check_style")
-  endif()
+        add_custom_target(${TARGET_NAME}
+                COMMAND Python::Interpreter ${OE_CPPLINT_COMMAND} "--quiet" ${SOURCES_LIST}
+                DEPENDS ${SOURCES_LIST}
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+        add_dependencies(check_style ${TARGET_NAME})
+        set_property(TARGET ${TARGET_NAME} PROPERTY FOLDER "check_style")
+    endif()
 endfunction()
 
 function(target_check_style TARGET)
-  if(CPPLINT_FOUND)
-    get_target_property(TARGET_SOURCES ${TARGET} SOURCES)
-
-    source_check_style(${TARGET} "${TARGET_SOURCES}")
-  endif()
+    if(OE_CHECK_STYLE)
+        get_target_property(TARGET_SOURCES ${TARGET} SOURCES)
+        source_check_style(${TARGET} "${TARGET_SOURCES}")
+    endif()
 endfunction()
-
-enable_check_style()
