8484727a6e0bee6427ac5eb3b7f8c3760b52faa9
Peter Skelton <peter.r.skelton@gmail.com>
2024-01-01 22:44:18 +0000

    Revert headless breaking change

--- src/Engine/Engine.cpp
+++ src/Engine/Engine.cpp
@@ -808,6 +808,9 @@ void Engine::SecondaryInitialization() {
         pUIAnims[i]->y = _4E98D0[i][2];
     }
 
+    // TODO(pskelton): dropping this causes std::bad_alloc in headless mode
+    UI_Create();
+
     spell_fx_renedrer->LoadAnimations();
 
     for (unsigned i = 0; i < 7; ++i) {
