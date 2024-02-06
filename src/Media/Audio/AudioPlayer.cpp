73acb4d6dec117295c0032905564d8cb5913272a
Peter Skelton <peter.r.skelton@gmail.com>
2024-02-06 22:20:21 +0000

    Add comment

--- src/Media/Audio/AudioPlayer.cpp
+++ src/Media/Audio/AudioPlayer.cpp
@@ -270,6 +270,8 @@ void AudioPlayer::playSound(SoundId eSoundID, SoundPlaybackMode mode, Pid pid) {
                                     pActors[object_id].pos.y,
                                     pActors[object_id].pos.z, MAX_SOUND_DIST);
 
+                // TODO(pskelton): Vanilla sounds like it does unique id but as exclusives
+                // Actors play unique sounds between them. Avoids issues where in a real time mob you are hit with a cacophony of overlapping attack noises.
                 result = _regularSoundPool.playUniqueSoundId(sample, si->dataSource, eSoundID, true);
 
                 break;
