5da00d021792dda1f413948e7ed97bb27536de51
Peter Skelton <peter.r.skelton@gmail.com>
2024-03-22 23:25:05 +0000

    Test data tag

--- test/Bin/GameTest/GameTests_1000.cpp
+++ test/Bin/GameTest/GameTests_1000.cpp
@@ -340,7 +340,7 @@ GAME_TEST(Issues, Issue1331) {
     EXPECT_EQ(pParty->pCharacters[2].GetRangedDamageString(), "41 - 45");
     auto damageRange = hpsTape.reversed().adjacentDeltas().flattened().filtered([] (int damage) { return damage > 0; }).minMax();
     // 2 -> 3 change here can happen. This just means that the Titans' physical resistance was never "lucky enough" to roll the damage down to 1 two times in a row.
-    EXPECT_EQ(damageRange, tape(/*1 * 2*/ 3, (43 + 13) * 2));
+    EXPECT_EQ(damageRange, tape(/*1 * 2*/ 3, (/*43*/45 + 13) * 2));
 }
 
 GAME_TEST(Issues, Issue1338) {
