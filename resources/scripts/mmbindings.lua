2cb534696cfb6d4cc1fc5d252f78ec6d46f6d1f5
Gerark <st4lk3r87@gmail.com>
2024-04-18 20:12:31 +0200

    Fix typos and missing line ending at the EOF

--- resources/scripts/mmbindings.lua
+++ resources/scripts/mmbindings.lua
@@ -1,5 +1,5 @@
 -- initMMBindings returns a table containing all the bindings related to the game
--- TODO(Gerark) In the future this comments will be replaced by a proper documentation ( maybe generated automatatically )
+-- TODO(Gerark) In the future these comments will be replaced by a proper documentation ( maybe generated automatically )
 -- mm.game - Generic game functions
 -- mm.party - Manipulate, Change or Inspect character stats
 -- mm.audio - Play music or sounds
