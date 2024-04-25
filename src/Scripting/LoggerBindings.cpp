e0ab92476e71b3948dfe0c6e95275e6269725c21
Gerark <st4lk3r87@gmail.com>
2024-04-25 22:16:44 +0200

    Use constant expr as first argument when logging

--- src/Scripting/LoggerBindings.cpp
+++ src/Scripting/LoggerBindings.cpp
@@ -14,12 +14,12 @@ LoggerBindings::LoggerBindings(const sol::state_view &solState, DistLogSink &dis
 sol::table LoggerBindings::getBindingTable() {
     if(!_bindingTable) {
         _bindingTable = _solState.create_table_with(
-            "info", [](std::string_view message) { logger->info(message); },
-            "trace", [](std::string_view message) { logger->trace(message); },
-            "debug", [](std::string_view message) { logger->debug(message); },
-            "warning", [](std::string_view message) { logger->warning(message); },
-            "error", [](std::string_view message) { logger->error(message); },
-            "critical", [](std::string_view message) { logger->critical(message); }
+            "info", [](std::string_view message) { logger->info("{}", message); },
+            "trace", [](std::string_view message) { logger->trace("{}", message); },
+            "debug", [](std::string_view message) { logger->debug("{}", message); },
+            "warning", [](std::string_view message) { logger->warning("{}", message); },
+            "error", [](std::string_view message) { logger->error("{}", message); },
+            "critical", [](std::string_view message) { logger->critical("{}", message); }
         );
     }
     return _bindingTable;
