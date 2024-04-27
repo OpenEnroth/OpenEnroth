eb4ec8ee5b426a549fe73dc5db6df940a19c66be
Gerark <st4lk3r87@gmail.com>
2024-04-27 23:02:53 +0200

    Uncomment the logSink code

--- src/Scripting/ScriptLogSink.cpp
+++ src/Scripting/ScriptLogSink.cpp
@@ -10,10 +10,10 @@ ScriptLogSink::ScriptLogSink(std::weak_ptr<sol::state_view> solState) : _solStat
 }
 
 void ScriptLogSink::write(const LogCategory& category, LogLevel level, std::string_view message) {
-    /*if (!_solState.expired()) {
+    if (!_solState.expired()) {
         sol::protected_function logSink = (*_solState.lock())["_globalLogSink"];
         if (logSink) {
             logSink(toString(level).c_str(), message);
         }
-    }*/
+    }
 }
