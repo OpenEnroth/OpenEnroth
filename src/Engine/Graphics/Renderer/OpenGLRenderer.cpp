58e7adeb3b62e35d04f038ff6c5a9cc956822a58
Gerark <st4lk3r87@gmail.com>
2024-05-12 18:57:24 +0200

    Use sdl ogl context when initializing imgui

--- src/Engine/Graphics/Renderer/OpenGLRenderer.cpp
+++ src/Engine/Graphics/Renderer/OpenGLRenderer.cpp
@@ -4661,7 +4661,7 @@ void OpenGLRenderer::_initImGui() {
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
 
     SDL_Window *sdlWindow = reinterpret_cast<SDL_Window *>(window->nativeHandle());
-    ImGui_ImplSDL2_InitForOpenGL(sdlWindow, nullptr);
+    ImGui_ImplSDL2_InitForOpenGL(sdlWindow, openGLContext->nativeHandle());
     ImGui_ImplOpenGL3_Init();
 }
 
