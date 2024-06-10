3092d79f522df9cb36bbea0994e266c45ee72749
captainurist <73941350+captainurist@users.noreply.github.com>
2024-06-10 19:20:06 +0100

    Code formatting

--- src/Library/FileSystem/Null/NullFileSystem.h
+++ src/Library/FileSystem/Null/NullFileSystem.h
@@ -12,9 +12,7 @@ class NullFileSystem : public ReadOnlyFileSystem {
  private:
     virtual bool _exists(const FileSystemPath &path) const override;
     virtual FileStat _stat(const FileSystemPath &path) const override;
-
     virtual std::vector<DirectoryEntry> _ls(const FileSystemPath &path) const override;
-
     virtual Blob _read(const FileSystemPath &path) const override;
     virtual std::unique_ptr<InputStream> _openForReading(const FileSystemPath &path) const override;
 
