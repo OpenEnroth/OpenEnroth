ed2a755879f124e44a6fe9655a7f2e9867f39cf1
captainurist <73941350+captainurist@users.noreply.github.com>
2024-06-10 19:54:11 +0100

    Split up File*Stream test files

--- src/Utility/Streams/Tests/FileInputStream_ut.cpp
+++ src/Utility/Streams/Tests/FileInputStream_ut.cpp
@@ -6,24 +6,25 @@
 #include "Utility/Streams/FileOutputStream.h"
 #include "Utility/Streams/FileInputStream.h"
 
-UNIT_TEST(FileOutputStream, Write) {
+UNIT_TEST(FileInputStream, Skip) {
     const char *tmpfile = "tmp_test.txt";
-    const char *tmpfilecontent = "1234\n";
-    size_t tmpfilesize = strlen(tmpfilecontent);
+    std::string data(3000, 'a');
 
     FileOutputStream out(tmpfile);
-    out.write(tmpfilecontent, tmpfilesize);
+    out.write(data.data(), data.size());
     out.close();
 
     FileInputStream in(tmpfile);
+    size_t bytes = in.skip(50);
+    EXPECT_EQ(bytes, 50);
+
+    bytes = in.skip(2000);
+    EXPECT_EQ(bytes, 2000);
 
     char buf[1024] = {};
-    size_t bytes = in.read(buf, 1024);;
-    EXPECT_EQ(bytes, 5);
-    EXPECT_EQ(strcmp(buf, tmpfilecontent), 0);
-
     bytes = in.read(buf, 1024);
-    EXPECT_EQ(bytes, 0);
+    EXPECT_EQ(bytes, 950);
+    EXPECT_EQ(std::string_view(buf, 950), std::string(950, 'a'));
     in.close();
 
     remove(tmpfile);
