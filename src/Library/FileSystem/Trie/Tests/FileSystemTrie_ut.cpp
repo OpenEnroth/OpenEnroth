a86e0ad92a05730061c5aab154b50b9c61b5ef97
captainurist <73941350+captainurist@users.noreply.github.com>
2024-06-10 21:54:02 +0100

    Fixed some problems in FileSystemTrie, added tests

--- src/Library/FileSystem/Trie/Tests/FileSystemTrie_ut.cpp
+++ src/Library/FileSystem/Trie/Tests/FileSystemTrie_ut.cpp
@@ -1,3 +1,6 @@
+#include <memory>
+#include <utility>
+
 #include "Testing/Unit/UnitTest.h"
 
 #include "Library/FileSystem/Trie/FileSystemTrie.h"
@@ -75,4 +78,39 @@ UNIT_TEST(FileSystemTrie, WalkClearsTail) {
     FileSystemPath tail1("a");
     trie.walk(FileSystemPath(""), &tail1);
     EXPECT_EQ(tail1, FileSystemPath());
+}
+
+UNIT_TEST(FileSystemTrie, ExtractInsert) {
+    using namespace detail; // NOLINT
+
+    FileSystemTrie<int> trie;
+    FileSystemTrieNode<int> *nodea = trie.insertOrAssign(FileSystemPath("a"), 10);
+
+    std::unique_ptr<FileSystemTrieNode<int>> extracted = trie.extract(nodea);
+    EXPECT_EQ(extracted->parent(), nullptr);
+    EXPECT_EQ(trie.find(FileSystemPath("a")), nullptr);
+
+    FileSystemTrieNode<int> *nodeb = trie.insertOrAssign(FileSystemPath("b"), std::move(extracted));
+    EXPECT_EQ(nodea, nodeb); // Checking that pointers are preserved.
+    EXPECT_EQ(nodeb->parent(), trie.root());
+
+    EXPECT_TRUE(trie.erase(nodeb));
+    EXPECT_TRUE(trie.isEmpty());
+}
+
+UNIT_TEST(FileSystemTrie, ExtractInsertRoot) {
+    using namespace detail; // NOLINT
+
+    FileSystemTrie<int> trie0;
+    FileSystemTrieNode<int> *node = trie0.insertOrAssign(FileSystemPath("a"), 10);
+    FileSystemTrieNode<int> *root0 = trie0.root();
+
+    FileSystemTrie<int> trie1;
+    trie1.insertOrAssign(FileSystemPath(), trie0.extract(root0));
+
+    EXPECT_NE(trie0.root(), nullptr); // New root was created.
+    EXPECT_TRUE(trie0.isEmpty());
+    EXPECT_FALSE(trie1.isEmpty());
+    EXPECT_EQ(trie1.root(), root0);
+    EXPECT_EQ(trie1.find(FileSystemPath("a")), node);
 }
