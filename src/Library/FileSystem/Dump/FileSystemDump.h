d2b88d4339a3d901eb9a9d46a2f0f427e4424dc3
captainurist <73941350+captainurist@users.noreply.github.com>
2024-06-11 22:34:20 +0100

    FileSystem dumper

---
+++ src/Library/FileSystem/Dump/FileSystemDump.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <cstdio>
+#include <cstdint>
+#include <cassert>
+#include <ostream>
+#include <vector>
+#include <string>
+#include <utility>
+
+#include "Library/FileSystem/Interface/FileSystem.h"
+
+#include "Utility/Memory/Blob.h"
+#include "Utility/Flags.h"
+
+enum class FileSystemDumpFlag {
+    FILE_SYSTEM_DUMP_WITH_CONTENTS = 1,
+};
+using enum FileSystemDumpFlag;
+MM_DECLARE_FLAGS(FileSystemDumpFlags, FileSystemDumpFlag)
+
+struct FileSystemDumpEntry {
+    std::string path;
+    FileType type;
+    std::int64_t size = 0;
+    Blob content; // If it's a regular file.
+
+    FileSystemDumpEntry(const FileSystemDumpEntry &other): path(other.path), type(other.type), size(other.size), content(Blob::share(other.content)) {}
+
+    FileSystemDumpEntry(std::string path, FileType type, std::int64_t size = 0, Blob content = {}): path(std::move(path)), type(type), size(size), content(std::move(content)) {
+        assert(type == FILE_REGULAR || type == FILE_DIRECTORY);
+        assert(content.empty() || content.size() == size);
+        assert(size == 0 || type == FILE_REGULAR);
+    }
+
+    FileSystemDumpEntry(std::string path, FileType type, std::string content) : path(std::move(path)), type(type) {
+        assert(type == FILE_REGULAR || type == FILE_DIRECTORY);
+        assert(content.empty() || type == FILE_REGULAR);
+
+        if (!content.empty()) {
+            size = content.size();
+            this->content = Blob::fromString(std::move(content));
+        }
+    }
+
+    friend bool operator==(const FileSystemDumpEntry &l, const FileSystemDumpEntry &r) {
+        return l.path == r.path && l.type == r.type && l.size == r.size && l.content.string_view() == r.content.string_view();
+    }
+};
+
+std::vector<FileSystemDumpEntry> dumpFileSystem(FileSystem *fs, FileSystemDumpFlags flags = 0, int maxEntries = 1024 * 1024);
+
+void dumpFileSystem(FILE *stream, FileSystem *fs, FileSystemDumpFlags flags = 0, int maxEntries = 1024 * 1024);
+
+// Google Test support.
+void PrintTo(const FileSystemDumpEntry &entry, std::ostream *stream);
