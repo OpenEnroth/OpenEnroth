ffc2891e4e87b38c39eddab7405da95eb760a6c0
captainurist <73941350+captainurist@users.noreply.github.com>
2024-06-11 22:36:42 +0100

    MergingFileSystem that's sane

--- src/Library/FileSystem/Merging/MergingFileSystem.cpp
+++ src/Library/FileSystem/Merging/MergingFileSystem.cpp
@@ -2,98 +2,79 @@
 
 #include <cassert>
 #include <utility>
-#include <unordered_map>
-#include <ranges>
 #include <string>
 #include <vector>
 #include <memory>
+#include <ranges>
+#include <tuple>
 
 #include "Library/FileSystem/Interface/FileSystemException.h"
 
-MergingFileSystem::MergingFileSystem(std::span<FileSystem *> bases) {
-    assert(!bases.empty());
-
-    std::ranges::copy(bases, std::back_inserter(_bases));
+MergingFileSystem::MergingFileSystem(std::vector<const FileSystem *> bases) {
+    _bases = std::move(bases);
 }
 
 MergingFileSystem::~MergingFileSystem() = default;
 
 bool MergingFileSystem::_exists(const FileSystemPath &path) const {
-    for (FileSystem *base : _bases)
+    for (const FileSystem *base : _bases)
         if (base->exists(path))
             return true;
     return false;
 }
 
 FileStat MergingFileSystem::_stat(const FileSystemPath &path) const {
-    for (FileSystem *base : _bases)
-        if (FileStat result = base->stat(path))
-            return result;
-    return {};
+    bool dirFound = false;
+    for (const FileSystem *base : _bases) {
+        FileStat stat = base->stat(path);
+        if (stat.type == FILE_REGULAR)
+            return stat; // Return the first file found, if any.
+        if (stat.type == FILE_DIRECTORY)
+            dirFound = true;
+    }
+    return dirFound ? FileStat(FILE_DIRECTORY, 0) : FileStat();
 }
 
 std::vector<DirectoryEntry> MergingFileSystem::_ls(const FileSystemPath &path) const {
-    std::unordered_map<std::string, FileType> mapping;
+    std::vector<DirectoryEntry> result;
 
     bool hasOne = false;
-    for (FileSystem *base : _bases) {
-        if (!base->exists(path))
+    for (const FileSystem *base : _bases) {
+        if (base->stat(path).type != FILE_DIRECTORY)
             continue;
 
+        // We will throw here if the folder was deleted between stat() and ls() calls. That's probably OK.
         hasOne = true;
-        for (DirectoryEntry &entry : base->ls(path))
-            mapping.emplace(std::move(entry.name), entry.type); // emplace doesn't replace if the element exists.
+        std::ranges::move(base->ls(path), std::back_inserter(result));
     }
 
-    if (!hasOne)
+    if (!hasOne) {
+        if (path.isEmpty())
+            return {};
         throw FileSystemException(FileSystemException::LS_FAILED_PATH_DOESNT_EXIST, path);
+    }
 
-    std::vector<DirectoryEntry> result;
-    for (auto &&[name, type] : mapping)
-        result.emplace_back(name, type);
+    std::ranges::sort(result, [] (const DirectoryEntry &l, const DirectoryEntry &r) {
+        return std::tie(l.name, l.type) < std::tie(r.name, r.type);
+    });
+    auto [tailStart, tailEnd] = std::ranges::unique(result);
+    result.erase(tailStart, tailEnd);
+
     return result;
 }
 
 Blob MergingFileSystem::_read(const FileSystemPath &path) const {
-    return locateForReading(path, true)->read(path);
-}
-
-void MergingFileSystem::_write(const FileSystemPath &path, const Blob &data) {
-    return _bases[0]->write(path, data);
+    return locateForReading(path)->read(path);
 }
 
 std::unique_ptr<InputStream> MergingFileSystem::_openForReading(const FileSystemPath &path) const {
-    return locateForReading(path, true)->openForReading(path);
+    return locateForReading(path)->openForReading(path);
 }
 
-std::unique_ptr<OutputStream> MergingFileSystem::_openForWriting(const FileSystemPath &path) {
-    return _bases[0]->openForWriting(path);
-}
-
-void MergingFileSystem::_rename(const FileSystemPath &srcPath, const FileSystemPath &dstPath) {
-    FileSystem *base = locateForReading(srcPath, false);
-    if (base == _bases[0]) {
-        base->rename(srcPath, dstPath);
-        _remove(srcPath); // Need to clean up other filesystems after a rename.
-    } else {
-        FileSystem::_rename(srcPath, dstPath); // Base filesystem will try to copy & remove.
-    }
-}
-
-bool MergingFileSystem::_remove(const FileSystemPath &path) {
-    bool result = false;
-    for (FileSystem *base : _bases)
-        result |= base->remove(path);
-    return result;
-}
-
-FileSystem *MergingFileSystem::locateForReading(const FileSystemPath &path, bool shouldExist) const {
-    for (FileSystem *base : _bases)
-        if (base->exists(path))
+const FileSystem *MergingFileSystem::locateForReading(const FileSystemPath &path) const {
+    for (const FileSystem *base : _bases)
+        if (base->stat(path).type == FILE_REGULAR)
             return base;
 
-    if (shouldExist)
-        throw FileSystemException(FileSystemException::READ_FAILED_PATH_DOESNT_EXIST, path);
-
-    return nullptr;
+    throw FileSystemException(FileSystemException::READ_FAILED_PATH_DOESNT_EXIST, path);
 }
--- src/Library/FileSystem/Merging/MergingFileSystem.h
+++ src/Library/FileSystem/Merging/MergingFileSystem.h
@@ -1,23 +1,47 @@
 #pragma once
 
 #include <vector>
-#include <string_view>
-#include <span>
 #include <memory>
 
-#include "Library/FileSystem/Interface/FileSystem.h"
+#include "Library/FileSystem/Interface/ReadOnlyFileSystem.h"
 
 /**
- * Merges several filesystems into one:
+ * Merges several filesystems into a single read-only view:
  * - Read operations go one by one over the underlying filesystems. This effectively means that files on filesystem #0
  *   will hide the files on filesystem #1 that have the same names.
- * - Write operations are always writing into filesystem #0.
- * - Remove removes from all filesystems.
- * - Rename tries to forward the call to filesystem #0 if possible, then removes the source file from all filesystems.
+ * - Folder contents are merged.
+ * - The resulting view is kind of a Schrodinger's filesystem because it can contain files and folders with the same
+ *   name.
+ * - In case there is a file and a folder with the same name, `stat()` returns information for a file, so that it's
+ *   possible to know its size. `ls()` will return both entries, in unspecified order.
+ *
+ * Some notes on why go Schrodingermaxxxing. When there is a conflict between underlying filesystems, and we have
+ * a file and a folder with the same names, we have several different options:
+ * 1. Only the file is visible.
+ * 2. Only the folder is visible.
+ * 3. Whichever is first in the filesystem list, is visible.
+ * 4. Conflicting path is not visible through the merged filesystem at all.
+ * 5. Schrodingermaxxx and make both visible.
+ * 
+ * When picking between the options, we must remember that:
+ * a. A tree of merging filesystems should behave the same way as a single flattened merging filesystem.
+ * b. Ideally, each method of the merging filesystem should call into each of the base filesystems at most once.
+ * c. The behavior shouldn't be surprising.
+ * 
+ * Point #1 above fails (b) and (c). We can fix (b) by introducing a `walk()` method to `FileSystem` that would
+ * return the longest existing prefix of a path, but then we're still left with the fact that behaviour is surprising.
+ * 
+ * Point #2 fails (c).
+ * 
+ * Point #3 fails (a), (b) and (c).
+ * 
+ * Point #4 fails (b), can be fixed in the same way as point #1. So, a viable option.
+ * 
+ * Point #5 satisfies all of the criteria, even though it's suffering a low-key bipolar disorder. So this is what we do.
  */
-class MergingFileSystem : public FileSystem {
+class MergingFileSystem : public ReadOnlyFileSystem {
  public:
-    explicit MergingFileSystem(std::span<FileSystem *> bases);
+    explicit MergingFileSystem(std::vector<const FileSystem *> bases);
     virtual ~MergingFileSystem();
 
  private:
@@ -25,14 +49,10 @@ class MergingFileSystem : public FileSystem {
     virtual FileStat _stat(const FileSystemPath &path) const override;
     virtual std::vector<DirectoryEntry> _ls(const FileSystemPath &path) const override;
     virtual Blob _read(const FileSystemPath &path) const override;
-    virtual void _write(const FileSystemPath &path, const Blob &data) override;
     virtual std::unique_ptr<InputStream> _openForReading(const FileSystemPath &path) const override;
-    virtual std::unique_ptr<OutputStream> _openForWriting(const FileSystemPath &path) override;
-    virtual void _rename(const FileSystemPath &srcPath, const FileSystemPath &dstPath) override;
-    virtual bool _remove(const FileSystemPath &path) override;
 
-    FileSystem *locateForReading(const FileSystemPath &path, bool shouldExist) const;
+    const FileSystem *locateForReading(const FileSystemPath &path) const;
 
  private:
-    std::vector<FileSystem *> _bases;
+    std::vector<const FileSystem *> _bases;
 };
