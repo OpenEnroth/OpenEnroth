9d97b76cfabd9bf23d958f7a73279f623d4ee5a7
captainurist <73941350+captainurist@users.noreply.github.com>
2024-07-21 22:25:42 +0000

    Deploying to gh-pages from @ OpenEnroth/OpenEnroth@2354330a3610bbb59d4a613f5653a1ce7191d67c 🚀

--- search/namespaces_2.js
+++ search/namespaces_2.js
@@ -1,10 +1,8 @@
 var searchData=
 [
   ['detail_0',['detail',['../namespacedetail.html',1,'']]],
-  ['detail_5fextension_1',['detail_extension',['../namespacedetail__extension.html',1,'']]],
-  ['detail_5ffb_5ferror_2',['detail_fb_error',['../namespacedetail__fb__error.html',1,'']]],
-  ['detail_5ffloat_3',['detail_float',['../namespacedetail__float.html',1,'']]],
-  ['detail_5fgl_5ferror_4',['detail_gl_error',['../namespacedetail__gl__error.html',1,'']]],
-  ['detail_5fintegral_5',['detail_integral',['../namespacedetail__integral.html',1,'']]],
-  ['detail_5fname_6',['detail_name',['../namespacedetail__name.html',1,'']]]
+  ['detail_5ffb_5ferror_1',['detail_fb_error',['../namespacedetail__fb__error.html',1,'']]],
+  ['detail_5ffloat_2',['detail_float',['../namespacedetail__float.html',1,'']]],
+  ['detail_5fgl_5ferror_3',['detail_gl_error',['../namespacedetail__gl__error.html',1,'']]],
+  ['detail_5fintegral_4',['detail_integral',['../namespacedetail__integral.html',1,'']]]
 ];
