1ae5ef229faa3ec0a9ce61f3cc07d23fb804f48a
Peter Skelton <peter.r.skelton@gmail.com>
2024-08-09 21:22:03 +0100

    Add todo

--- src/Engine/Objects/Actor.cpp
+++ src/Engine/Objects/Actor.cpp
@@ -540,6 +540,7 @@ void Actor::AI_SpellAttack(unsigned int uActorID, AIDirection *pDir,
             break;
 
         case SPELL_LIGHT_PARALYZE:
+            // TODO(pskelton): This is a vanilla bug - monsters with instant targeting spells can't actually use them - fix properly??
             logger->warning("Spell Paralyze cast - replaced with dispel");
             [[fallthrough]];
         case SPELL_LIGHT_DISPEL_MAGIC:
