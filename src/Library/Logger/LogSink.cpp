14eab3c5fe444a810e2a2b8bc5b4430412bad9a5
captainurist <73941350+captainurist@users.noreply.github.com>
2024-10-22 22:43:24 +0100

    Use custom MsvcSinkSt

--- src/Library/Logger/LogSink.cpp
+++ src/Library/Logger/LogSink.cpp
@@ -7,10 +7,17 @@
 #include <unordered_map>
 #include <memory>
 
+#ifdef _WINDOWS
+#   define WIN32_LEAN_AND_MEAN
+#   include <Windows.h>
+#endif
+
 #include <spdlog/sinks/android_sink.h> // NOLINT
 #include <spdlog/sinks/dist_sink.h> // NOLINT
 #include <spdlog/sinks/msvc_sink.h> // NOLINT
 #include <spdlog/sinks/stdout_color_sinks.h> // NOLINT
+
+#include "Utility/Win/Unicode.h"
 
 #include "LogEnumFunctions.h"
 
@@ -52,12 +59,35 @@ class AndroidSinkSt : public LogSink {
 };
 #endif
 
+#ifdef _WINDOWS
+/**
+ * Need a custom sink here b/c `spdlog::sinks::msvc_sink_st` throws an exception on invalid UTF8, and this is totally
+ * NOT OK.
+ */
+class MsvcSinkSt : public spdlog::sinks::base_sink<spdlog::details::null_mutex> {
+ public:
+    MsvcSinkSt() = default;
+
+ protected:
+    void sink_it_(const spdlog::details::log_msg &msg) override {
+        if (!IsDebuggerPresent())
+            return;
+
+        spdlog::memory_buf_t formatted;
+        formatter_->format(msg, formatted);
+        OutputDebugStringW(win::toUtf16(std::string_view(formatted.data(), formatted.size())).data());
+    }
+
+    void flush_() override {}
+};
+#endif
+
 std::unique_ptr<LogSink> LogSink::createDefaultSink() {
 #if defined(__ANDROID__)
     return std::make_unique<AndroidSinkSt>();
 #elif defined(_WINDOWS)
     std::vector<std::shared_ptr<spdlog::sinks::sink>> sinks = {
-        std::make_shared<spdlog::sinks::msvc_sink_st>(/*check_debugger_present=*/true),
+        std::make_shared<MsvcSinkSt>(),
         std::make_shared<spdlog::sinks::stderr_color_sink_st>()
     };
     return std::make_unique<SpdlogSink<spdlog::sinks::dist_sink_st>>(std::move(sinks));
