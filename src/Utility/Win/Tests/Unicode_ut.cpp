c5e43f7217601e492ec8a625822519c5cadf3b8b
captainurist <73941350+captainurist@users.noreply.github.com>
2024-10-23 18:31:53 +0100

    Add another unicode test

--- src/Utility/Win/Tests/Unicode_ut.cpp
+++ src/Utility/Win/Tests/Unicode_ut.cpp
@@ -8,6 +8,10 @@ UNIT_TEST(Unicode, InvalidUtf8) {
     EXPECT_EQ(win::toUtf16("\xc3\x28"), L"\xfffd(");
 }
 
+UNIT_TEST(Unicode, InvalidUtf16) {
+    EXPECT_EQ(win::toUtf8(L"\xDC00\x0028"), "\xef\xbf\xbd("); // \xef\xbf\xb is \xfffd.
+}
+
 UNIT_TEST(Unicode, Empty) {
     EXPECT_EQ(win::toUtf16(""), L"");
     EXPECT_EQ(win::toUtf8(L""), "");
