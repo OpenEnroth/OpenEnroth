ed3c7617f01c59289bec8f7adfd5e8658f4ffd37
captainurist <73941350+captainurist@users.noreply.github.com>
2024-11-02 22:39:38 +0000

    Docs in RotatingLogSink

--- src/Library/Logger/RotatingLogSink.cpp
+++ src/Library/Logger/RotatingLogSink.cpp
@@ -23,9 +23,14 @@ std::unique_ptr<OutputStream> RotatingLogSink::openRotatingStream(const FileSyst
         // We're being lazy here and just checking stem & extension. Can do a regex, but that would be an overkill.
         return !entry.name.starts_with(components.stem()) && !entry.name.ends_with(components.extension());
     });
-    std::ranges::sort(entries, std::ranges::greater());
 
     // Drop old log files.
+    //
+    // This will work in unexpected ways when traveling between time zones, playing with system clock, or starting OE
+    // when DST moves the clock back 1 hour. Should we care? I don't think so. A better implementation would be to add
+    // time to `FileStat`, and look at it instead. Originally we had a time field there, but it was dropped, and I'm
+    // not keen on bringing it back.
+    std::ranges::sort(entries, std::ranges::greater());
     while (!entries.empty() && entries.size() >= count) {
         fs->remove(components.prefix() / entries.back().name);
         entries.pop_back();
