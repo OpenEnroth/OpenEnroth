b45dc0581af7f3437f9a264b22834e3a19dbcdd6
captainurist <73941350+captainurist@users.noreply.github.com>
2025-02-09 14:31:06 +0000

    Drop unused code from NullPlatform

--- src/Library/Platform/Null/NullPlatformSharedState.h
+++ src/Library/Platform/Null/NullPlatformSharedState.h
@@ -14,12 +14,10 @@ class NullPlatformSharedState {
     explicit NullPlatformSharedState(NullPlatformOptions options) {
         assert(!options.displayGeometries.empty());
 
-        cursorPos = options.displayGeometries[0].topLeft();
         this->options = std::move(options);
     }
 
     NullPlatformOptions options;
     std::unordered_map<std::thread::id, NullOpenGLContext *> contextByThreadId;
     bool cursorShown = true;
-    Pointi cursorPos;
 };
