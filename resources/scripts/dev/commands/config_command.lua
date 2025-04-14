5892da5ed3539ff8992d24c5413ff2dce377d38d
captainurist <73941350+captainurist@users.noreply.github.com>
2025-04-14 21:55:23 +0100

    Fix game config changes via debug console

--- resources/scripts/dev/commands/config_command.lua
+++ resources/scripts/dev/commands/config_command.lua
@@ -2,60 +2,52 @@ local Config = require "bindings.config"
 local Utilities = require "utils"
 
 ---Get the value of a configEntry
----@param configName string    - config entry name
----@param sectionName? string  - section of the config entry
+---@param param1 string         - config entry name OR config section name
+---@param param2? string        - config entry name if you are passing in section name as the first parameter
 ---@return string               - the message sent back to the console
 ---@return boolean              - flag that tells if the command has been successful
-local function getConfig(configName, sectionName)
+local function getConfig(param1, param2)
     local value = nil
     local message = ""
-    if sectionName ~= nil then
-        value = Config.getConfig(sectionName, configName)
-        message = "[" .. sectionName .. "] - " .. configName .. ": "
+    if param2 ~= nil then
+        value = Config.getConfig(param1, param2)
+        message = param1 .. "." .. param2 .. ": "
     else
-        value = Config.getConfig(configName)
-        message = configName .. ": "
+        value = Config.getConfig(param1)
+        message = param1 .. ": "
     end
     return message .. tostring(value), true
 end
 
 ---Change the value of the configEntry
----@param configName string - name of the configEntry
----@param param2 string      - section of the config entry. If param3 is nil param2 represents the value instead
----@param param3? string     - new value to apply
+---@param param1 string      - config entry name OR config section name
+---@param param2 string      - config entry name if you are passing in section name as the first parameter, new value otherwise
+---@param param3? string     - new value if you are passing in section name as the first parameter, nil otherwise
 ---@return string            - the message sent back to the console
 ---@return boolean           - flag that tells if the command has been successful
-local function setConfig(configName, param2, param3)
+local function setConfig(param1, param2, param3)
     local value = nil
     local message = ""
-    local sectionName = nil
     if param3 ~= nil then
-        sectionName = param2
-        value = param3
+        Config.setConfig(param1, param2, param3)
+        value = Config.getConfig(param1, param2)
+        message = param1 .. "." .. param2 .. ": "
     else
-        value = param2
-    end
-
-    if sectionName ~= nil then
-        Config.setConfig(sectionName, configName, value)
-        value = Config.getConfig(sectionName, configName)
-        message = "[" .. sectionName .. "] - " .. configName .. ": "
-    else
-        Config.setConfig(configName, value)
-        value = Config.getConfig(configName)
-        message = configName .. ": "
+        Config.setConfig(param1, param2)
+        value = Config.getConfig(param1)
+        message = param1 .. ": "
     end
     return message .. tostring(value), true
 end
 
 ---Toggle the boolean config value
----@param configName string - name of the configEntry
+---@param entry string       - name of the config entry
 ---@return string            - the message sent back to the console
 ---@return boolean           - flag that tells if the command has been successful
-local function toggleConfig(configName)
-    local currentValueStr = Config.getConfig(configName)
+local function toggleConfig(entry)
+    local currentValueStr = Config.getConfig(entry)
     local value = Utilities.toBoolean(currentValueStr)
-    return setConfig(configName, tostring(not value))
+    return setConfig(entry, tostring(not value))
 end
 
 local subCommands = {
