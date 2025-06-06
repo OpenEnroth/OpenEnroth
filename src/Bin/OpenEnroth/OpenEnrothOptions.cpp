e0e2c39982522f62739895b514398fa442ab8be5
captainurist <73941350+captainurist@users.noreply.github.com>
2025-06-06 21:07:48 +0100

    Can use --tracing-rng with OpenEnroth play

--- src/Bin/OpenEnroth/OpenEnrothOptions.cpp
+++ src/Bin/OpenEnroth/OpenEnrothOptions.cpp
@@ -37,6 +37,9 @@ OpenEnrothOptions OpenEnrothOptions::parse(int argc, char **argv) {
         "--portable", portable,
         "Run in portable mode, game & user data paths will default to current folder. "
         "If '.portable' file exists in the current folder, then this parameter defaults to 'true'.");
+    app->add_flag(
+        "--tracing-rng", result.tracingRng,
+        "Use random number generators that print stack trace on each call.");
     app->add_option(
         "--log-level", result.logLevel,
         "Log level, one of 'none', 'trace', 'debug', 'info', 'warning', 'error', 'critical'.")->option_text("LOG_LEVEL");
@@ -59,9 +62,6 @@ OpenEnrothOptions OpenEnrothOptions::parse(int argc, char **argv) {
     app->add_flag(
         "--headless", result.headless,
         "Run in headless mode.");
-    retrace->add_flag(
-        "--tracing-rng", result.tracingRng,
-        "Use random number generators that print stack trace on each call.");
     retrace->add_flag(
         "--check-canonical", result.retrace.checkCanonical,
         "Check whether all passed traces are stored in canonical representation and return an error if not. Don't overwrite the actual trace files.");
