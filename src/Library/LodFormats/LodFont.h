f2ed8a6b7fcb6e39e8d60eb9dc1b3dd88ecd3d45
captainurist <73941350+captainurist@users.noreply.github.com>
2025-06-26 15:56:55 +0100

    Decode lod font into two files

--- src/Library/LodFormats/LodFont.h
+++ src/Library/LodFormats/LodFont.h
@@ -19,9 +19,9 @@ struct LodFontHeader {
 };
 
 struct LodFontMetrics {
-    int leftSpacing = 0; // Spacing in pixels to the left of the character.
+    int leftSpacing = 0; // Spacing in pixels to the left of the character. Can be negative.
     int width = 0; // Width of the character image.
-    int rightSpacing = 0; // Spacing in pixels to the right of the character.
+    int rightSpacing = 0; // Spacing in pixels to the right of the character. Can be negative.
 };
 
 struct LodFontAtlas {
@@ -31,32 +31,37 @@ struct LodFontAtlas {
 
 class LodFont {
  public:
-    int height() const {
+    [[nodiscard]] int height() const {
         return _header.fontHeight;
     }
 
-    bool supports(int c) const {
+    [[nodiscard]] bool supports(int c) const {
         return c >= _header.firstChar && c <= _header.lastChar;
     }
-    bool supports(char c) const = delete; // Don't call with char as it can be signed.
-
-    const LodFontMetrics &metrics(int c) const {
-        return _atlas.metrics[c];
-    }
-    const LodFontMetrics &metrics(char c) const = delete; // Don't call with char as it can be signed.
+    [[nodiscard]] bool supports(char c) const = delete; // Don't call with char as it can be signed.
 
     /**
-     * @param c                         Character to get image for.
+     * @param c                         Character to get metrics for, must be in `[0, 255]`.
+     * @return                          Font glyph metrics for the character. Will return all zeros for chars that are
+     *                                  not supported.
+     */
+    [[nodiscard]] const LodFontMetrics &metrics(int c) const {
+        return _atlas.metrics[c];
+    }
+    [[nodiscard]] const LodFontMetrics &metrics(char c) const = delete; // Don't call with char as it can be signed.
+
+    /**
+     * @param c                         Character to get image for, must be in `[0, 255]`.
      * @return                          Character image. Pixels are `0` for background, `1` for shadow, `255` for text.
      *                                  Will return an empty image for chars that are not supported.
      */
-    GrayscaleImageView image(int c) const {
+    [[nodiscard]] GrayscaleImageView image(int c) const {
         return GrayscaleImageView(
             static_cast<const uint8_t *>(_pixels.data()) + _atlas.offsets[c],
             _atlas.metrics[c].width,
             _header.fontHeight);
     }
-    GrayscaleImageView image(char c) const = delete; // Don't call with char as it can be signed.
+    [[nodiscard]] GrayscaleImageView image(char c) const = delete; // Don't call with char as it can be signed.
 
     friend LodFont lod::decodeFont(const Blob &blob);
 
