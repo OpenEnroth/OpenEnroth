a94b4f62be9a6cfca6ab81708f8abfb091e114f9
captainurist <73941350+captainurist@users.noreply.github.com>
2025-06-30 10:51:25 +0000

    Deploying to gh-pages from @ OpenEnroth/OpenEnroth@7d2d26b67b1fde763317fe8562f5c4f0f2b8b4b9 🚀

--- search/related_5.js
+++ search/related_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['logger_0',['Logger',['../classLogCategory.html#aff02b76416d2846736b7ecd798921a0a',1,'LogCategory']]]
+  ['decodefont_0',['decodeFont',['../classLodFont.html#a0086018107a91d54f2d6033c840c5f7e',1,'LodFont']]],
+  ['logger_1',['Logger',['../classLogCategory.html#aff02b76416d2846736b7ecd798921a0a',1,'LogCategory']]]
 ];
