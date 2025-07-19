df978aa41989fea56dcf1d91e87591bcf0ea3fc3
captainurist <73941350+captainurist@users.noreply.github.com>
2025-07-19 16:00:42 +0100

    Docs for GUIFont

--- src/GUI/GUIFont.h
+++ src/GUI/GUIFont.h
@@ -13,6 +13,18 @@
 class GUIWindow;
 class GraphicsImage;
 
+/**
+ * Some notes on markup characters supported by `GUIFont` functions.
+ *
+ * - `\n` is a regular line feed.
+ * - `\tXXX`, where `XXX` is decimal offset in pixels. Moves the caret to a position offset by `XXX` to the right from
+ *   the start of the line. This is how aligned tables are implemented.
+ * - `\rXXX`, where `XXX` is a decimal offset in pixels. Moves the caret so that the rest of the line is
+ *   right-justified, with the given offset from the right window border. This is used e.g. in the create party screen.
+ * - `\fXXXXX`, where `XXXXX` is a decimal color code for 16-bit color to use for the text that follows.
+ *
+ * @see Color::fromC16
+ */
 class GUIFont {
  public:
     GUIFont();
@@ -41,7 +53,7 @@ class GUIFont {
      * @param text                          Input line of text.
      * @param color                         Color that the text should be started to be drawn at - this allows feeding
      *                                      in the color returned from the previous call to maintain correct color when
-     *                                      its split onto a new line.
+     *                                      it's split onto a new line.
      * @param defaultColor                  The color that the text should return to on hitting a default color tag.
      * @param position                      Position to draw the text line to.
      * @param max_len_pix                   The maximum allowed width for this line of text.
