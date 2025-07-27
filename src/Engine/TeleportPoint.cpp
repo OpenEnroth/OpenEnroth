615fbd2e5c0d6d8598fcb978235f1e6a6affb177
Peter Skelton <peter.r.skelton@gmail.com>
2025-07-27 16:24:40 +0100

    Add position check before teleport

--- src/Engine/TeleportPoint.cpp
+++ src/Engine/TeleportPoint.cpp
@@ -1,6 +1,11 @@
 #include "TeleportPoint.h"
 
 #include "Engine/Party.h"
+#include "Engine/Graphics/Indoor.h"
+#include "Engine/Graphics/Outdoor.h"
+#include "engine/Graphics/LocationFunctions.h"
+
+#include "Library/Logger/Logger.h"
 
 void TeleportPoint::invalidate() {
     _teleportValid = false;
@@ -18,6 +23,24 @@ void TeleportPoint::setTeleportTarget(Vec3f pos, int yaw, int pitch, int zSpeed)
 
 void TeleportPoint::doTeleport(bool keepOnZero) {
     assert(_teleportValid);
+
+    // Test target position is valid
+    if (uCurrentlyLoadedLevelType == LEVEL_INDOOR) {
+        if (!pIndoor->GetSector(_pos)) {
+            logger->warning("TeleportPoint::doTeleport - Cannot GetSector for target position ({}, {}, {}), skipping teleport", _pos.x, _pos.y, _pos.z);
+            return;
+        }
+    } else {
+        bool partyIsOnWater = false;
+        int floorFaceId = -1;
+        float newFloorLevel = ODM_GetFloorLevel(_pos, &partyIsOnWater, &floorFaceId) + 1;
+        if (_pos.x < -maxPartyAxisDistance || _pos.x > maxPartyAxisDistance ||
+            _pos.y < -maxPartyAxisDistance || _pos.y > maxPartyAxisDistance ||
+            _pos.z < newFloorLevel) {
+            logger->warning("TeleportPoint::doTeleport - Target position ({}, {}, {}) is out of bounds, skipping teleport", _pos.x, _pos.y, _pos.z);
+            return;
+        }
+    }
 
     Vec3f newPos = pParty->pos;
     Vec3f newSpeed = pParty->velocity;
