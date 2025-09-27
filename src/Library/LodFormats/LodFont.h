8164218a463bb2e697003d0b0dc5fd1cdd19e13e
captainurist <73941350+captainurist@users.noreply.github.com>
2025-09-27 22:14:20 +0200

    Use char in LodFont API

--- src/Library/LodFormats/LodFont.h
+++ src/Library/LodFormats/LodFont.h
@@ -35,33 +35,30 @@ class LodFont {
         return _header.fontHeight;
     }
 
-    [[nodiscard]] bool supports(int c) const {
-        return c >= _header.firstChar && c <= _header.lastChar;
+    [[nodiscard]] bool supports(char c) const {
+        return static_cast<unsigned char>(c) >= _header.firstChar && static_cast<unsigned char>(c) <= _header.lastChar;
     }
-    [[nodiscard]] bool supports(char c) const = delete; // Don't call with char as it can be signed.
 
     /**
      * @param c                         Character to get metrics for, must be in `[0, 255]`.
      * @return                          Font glyph metrics for the character. Will return all zeros for chars that are
      *                                  not supported.
      */
-    [[nodiscard]] const LodFontMetrics &metrics(int c) const {
-        return _atlas.metrics[c];
+    [[nodiscard]] const LodFontMetrics &metrics(char c) const {
+        return _atlas.metrics[static_cast<unsigned char>(c)];
     }
-    [[nodiscard]] const LodFontMetrics &metrics(char c) const = delete; // Don't call with char as it can be signed.
 
     /**
      * @param c                         Character to get image for, must be in `[0, 255]`.
      * @return                          Character image. Pixels are `0` for background, `1` for shadow, `255` for text.
      *                                  Will return an empty image for chars that are not supported.
      */
-    [[nodiscard]] GrayscaleImageView image(int c) const {
+    [[nodiscard]] GrayscaleImageView image(char c) const {
         return GrayscaleImageView(
-            static_cast<const uint8_t *>(_pixels.data()) + _atlas.offsets[c],
-            _atlas.metrics[c].width,
+            static_cast<const uint8_t *>(_pixels.data()) + _atlas.offsets[static_cast<unsigned char>(c)],
+            _atlas.metrics[static_cast<unsigned char>(c)].width,
             _header.fontHeight);
     }
-    [[nodiscard]] GrayscaleImageView image(char c) const = delete; // Don't call with char as it can be signed.
 
     friend LodFont lod::decodeFont(const Blob &blob);
 
