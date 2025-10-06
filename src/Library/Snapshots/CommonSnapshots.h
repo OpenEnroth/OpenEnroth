8e9c2357d53689c9eb966be1180a998cb6fb6045
Peter Skelton <peter.r.skelton@gmail.com>
2025-10-06 20:48:02 +0100

    Add clear to snapshots too

--- src/Library/Snapshots/CommonSnapshots.h
+++ src/Library/Snapshots/CommonSnapshots.h
@@ -79,6 +79,7 @@ void reconstruct(const std::array<char, N> &src, std::string *dst) {
 
 template<ResizableContiguousContainer Src, ResizableContiguousContainer Dst, class... Tags> requires DifferentElementTypes<Src, Dst>
 void snapshot(const Src &src, Dst *dst, const Tags &... tags) {
+    dst->clear();
     dst->resize(src.size());
 
     std::span srcSpan(src.data(), src.size());
@@ -104,6 +105,7 @@ void reconstruct(const Src &src, Dst *dst, const Tags &... tags) {
 
 template<class T1, class T2, class... Tags>
 void snapshot(const std::deque<T1> &src, std::vector<T2> *dst, const Tags &... tags) {
+    dst->clear();
     dst->resize(src.size());
 
     for (size_t i = 0; i < src.size(); i++)
