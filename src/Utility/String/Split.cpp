0147b4b58519c5bc7dd17b811c057e710c6e5eb6
captainurist <73941350+captainurist@users.noreply.github.com>
2025-10-11 09:33:17 +0200

    Better string splitting

--- src/Utility/String/Split.cpp
+++ src/Utility/String/Split.cpp
@@ -3,9 +3,4 @@
 #include <string_view>
 #include <vector>
 
-void split(std::string_view s, char sep, std::vector<std::string_view> *result) {
-    result->clear();
-    result->reserve(16);
-    for (std::string_view chunk : split(s, sep))
-        result->push_back(chunk);
-}
+// All split functionality is now in the header via templates.
