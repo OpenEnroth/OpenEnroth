034908c8e79e923e2ba88e46e80073ee44237443
pskelton <pskelton@users.noreply.github.com>
2025-12-29 16:56:53 +0000

    Deploying to gh-pages from @ OpenEnroth/OpenEnroth@54e48ccd809123990b632d86fbd22c32a5a7c980 🚀

--- search/typedefs_e.js
+++ search/typedefs_e.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['randomenginesarray_0',['RandomEnginesArray',['../classEngineRandomComponent.html#a01182917ea13f89d841cde1c2169c69f',1,'EngineRandomComponent']]],
-  ['recti_1',['Recti',['../Rect_8h.html#a32073cc1967b01302be5d39e09079649',1,'Rect.h']]],
-  ['reference_2',['reference',['../classdetail_1_1RangeIterator.html#ae8c8fbfa4ba36b2ce4382fa136b792db',1,'detail::RangeIterator::reference()'],['../classSegment.html#af0ff246cdf8ecd8984307446636ea09c',1,'Segment::reference()']]],
-  ['rgbaimage_3',['RgbaImage',['../Library_2Image_2Image_8h.html#ada8411436fef9ac499121bebb98f43cd',1,'Image.h']]],
-  ['rgbaimageview_4',['RgbaImageView',['../Library_2Image_2Image_8h.html#ab46117a6bb4ec93c50bc59858278d567',1,'Image.h']]]
+  ['rectf_1',['Rectf',['../Rect_8h.html#a606a8ae210181d2c740dca82572103ff',1,'Rect.h']]],
+  ['recti_2',['Recti',['../Rect_8h.html#a32073cc1967b01302be5d39e09079649',1,'Rect.h']]],
+  ['reference_3',['reference',['../classdetail_1_1RangeIterator.html#ae8c8fbfa4ba36b2ce4382fa136b792db',1,'detail::RangeIterator::reference()'],['../classSegment.html#af0ff246cdf8ecd8984307446636ea09c',1,'Segment::reference()']]],
+  ['rgbaimage_4',['RgbaImage',['../Library_2Image_2Image_8h.html#ada8411436fef9ac499121bebb98f43cd',1,'Image.h']]],
+  ['rgbaimageview_5',['RgbaImageView',['../Library_2Image_2Image_8h.html#ab46117a6bb4ec93c50bc59858278d567',1,'Image.h']]]
 ];
