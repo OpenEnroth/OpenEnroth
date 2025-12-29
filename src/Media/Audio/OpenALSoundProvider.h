819052ef6f1c05cb97ab1e801492aa38c1794f89
captainurist <73941350+captainurist@users.noreply.github.com>
2025-12-29 10:38:09 +0100

    Dropped dead code in OpenALSoundProvider (#2328)

--- src/Media/Audio/OpenALSoundProvider.h
+++ src/Media/Audio/OpenALSoundProvider.h
@@ -12,11 +12,6 @@ constexpr float ROLLOFF_FACTOR = 1.5f;
 
 class OpenALSoundProvider {
  public:
-    struct TrackBuffer {
-        unsigned int source_id;
-        unsigned int buffer_id;
-    };
-
     struct StreamingTrackBuffer {
         unsigned int source_id;
         ALenum sample_format;
@@ -30,16 +25,11 @@ class OpenALSoundProvider {
     void Release();
 
     void DeleteStreamingTrack(StreamingTrackBuffer **buffer);
-    void DeleteBuffer16(TrackBuffer **buffer);
-    float alBufferLength(unsigned int buffer);
     StreamingTrackBuffer *CreateStreamingTrack16(int num_channels,
                                                  int sample_rate,
                                                  int bytes_per_sample);
     void Stream16(StreamingTrackBuffer *buffer, int num_samples,
                   const void *samples, bool wait = false);
-    TrackBuffer *CreateTrack16(int num_channels, int sample_rate,
-                               const void *data, size_t size);
-    void PlayTrack16(TrackBuffer *buffer, bool loop = false, bool wait = false);
     void SetListenerPosition(float x, float y, float z);
     void SetOrientation(float yaw, float pitch);
 
