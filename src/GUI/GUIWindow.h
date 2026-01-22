0cc772e8bea89d7962dab04459d2ff9cab8a22b5
Peter Skelton <peter.r.skelton@gmail.com>
2026-01-22 22:37:25 +0000

    Window counting for mouselook

--- src/GUI/GUIWindow.h
+++ src/GUI/GUIWindow.h
@@ -109,6 +109,7 @@ class GUIWindow {
     std::vector<GUIButton*> vButtons;
 
     std::shared_ptr<Io::Mouse> mouse = nullptr;
+    bool _requireMousePointer = false;
 };
 
 class OnButtonClick : public GUIWindow {
