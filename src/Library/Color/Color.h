0868b1030adee60dff2b8a5f2403ae6d79ae69b1
captainurist <73941350+captainurist@users.noreply.github.com>
2026-02-01 01:09:21 +0100

    Updated fmt and spdlog (#2347)

--- src/Library/Color/Color.h
+++ src/Library/Color/Color.h
@@ -108,7 +108,7 @@ struct fmt::formatter<ColorTag> {
         auto pos = ctx.begin();
         auto end = ctx.end();
         if (pos == end || *pos != ':' || ++pos == end || *pos != '}')
-            fmt::detail::throw_format_error("ColorTag needs {::} format specifier");
+            fmt::report_error("ColorTag needs {::} format specifier");
         return pos; // pos points to '}'
     }
 
--- src/Utility/String/Format.h
+++ src/Utility/String/Format.h
@@ -4,5 +4,6 @@
 #include <fmt/format.h>
 #include <fmt/compile.h>
 #include <fmt/chrono.h>
+#include <fmt/ranges.h>
 
 using namespace fmt::literals; // NOLINT
