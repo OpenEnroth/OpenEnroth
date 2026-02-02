ea1a73673bebabffc4a6caab210450a6e5f7f13e
captainurist <73941350+captainurist@users.noreply.github.com>
2026-02-02 15:52:49 +0000

    Simpler binary serialization tags

--- src/Library/Binary/BinaryConcepts.h
+++ src/Library/Binary/BinaryConcepts.h
@@ -3,80 +3,29 @@
 #include <concepts>
 #include <type_traits>
 
-template<class T>
-struct is_proxy_binary_source : std::false_type {};
-
-template<class T>
-struct is_proxy_binary_sink : std::false_type {};
-
-template<class T>
-struct is_proxy_binarizable : std::false_type {};
-
 /**
- * Binary serialization provides a set of overloads, and when there's too many, a moment comes when they need to be
- * explicitly ordered. Consider the following overloads:
- * ```
- * template<class T, class... Tag>
- * void serialize(const T &src, Blob *dst, const Tag &...); // #1
- * template<class Src, class Dst, class Via>
- * void serialize(const Src &src, Dst *dst, ViaTag<Via>); // #2
- * ```
+ * Type trait for greedy tags. Greedy tags should be processed before any source/destination conversions. Examples
+ * include `ViaTag`.
  *
- * And the following call:
- * ```
- * Blob blob;
- * serialize(SomeData{}, &blob, tags::via<SomeData_Bin>);
- * ```
- *
- * This call is ambiguous because the types provided match both overloads, with neither one being a better match.
- * Besides, the ambiguity is genuine here - the order really doesn't matter, so whether `#1` calls into `#2` or the
- * other way around, result stays the same. However, this is C++, so we need to:
- * 1. Pick the order we like.
- * 2. Guide the compiler into it.
- *
- * In this particular case it makes sense to disambiguate on `src` and `dst` first, and then process all the tags.
- * How to do it? We can introduce the notion of 'proxy' binary serialization types:
- * - Proxy source is a type that can be deserialized-from, and that simply replaces the `src` with another type
- *   and forwards to another `deserialize` overload.
- * - Proxy sink is a type that can be serialized-into, and that simply replaces the `dst` with another type
- *   and forwards to another `serialize` overload.
- * - Proxy binarizable types are basically the same thing, but for client types that are serialized into binary form /
- *   deserialized from binary form by forwarding the corresponding call to another type. Note that calling serialize /
- *   deserialize for members doesn't mean that a type is a proxy.
- *
- * Thus, proxy types stand in contrast to regular, or "leaf" types - types for which non-template overloads are
- * provided. All overloads that process tags require regular `src` and `dst` arguments, and this provides natural
- * ordering:
- * 1. First, all `src` / `dst` transformations are carried out.
- * 2. Then, tags are applied in order.
- *
- * To achieve this, the following concepts are provided:
- * - `RegularBinarySource` to denote a non-proxy binary source (e.g., an `InputStream`).
- * - `RegularBinarySink` to denote a non-proxy binary sink (e.g., an `OutputStream`).
- * - `RegularBinarizable` to denote a non-proxy client type to be serialized or deserialized.
- *
- * Then, for the types that actually are proxies, the following type traits are provided for specialization:
- * - `is_proxy_binary_source` for proxy binary sources.
- * - `is_proxy_binary_sink` for proxy binary sinks.
- * - `is_proxy_binarizable` for proxy client types.
- *
- * @see RegularBinarySink
- * @see RegularBinarizable
+ * Regular tags (like `ContextTag`) just pass through and don't need special ordering.
  */
 template<class T>
-concept RegularBinarySource = !is_proxy_binary_source<std::remove_cvref_t<T>>::value;
+struct is_greedy_tag : std::false_type {};
 
 /**
- * @see RegularBinarySource
+ * Checks if the first type in a parameter pack satisfies a given type trait.
+ *
+ * Returns `false` for an empty pack, otherwise returns `Condition<First>::value` where `First` is the first type
+ * in the pack (with cv-ref qualifiers removed).
+ *
+ * @tparam Condition                    Type trait to check, e.g. `is_greedy_tag`.
+ * @tparam Args                         Parameter pack to check.
  */
-template<class T>
-concept RegularBinarySink = !is_proxy_binary_sink<std::remove_cvref_t<T>>::value;
+template<template<class> class Condition, typename... Args>
+constexpr bool starts_with_v = false;
 
-/**
- * @see RegularBinarySource
- */
-template<class T>
-concept RegularBinarizable = !is_proxy_binarizable<std::remove_cvref_t<T>>::value;
+template<template<class> class Condition, class First, class... Rest>
+constexpr bool starts_with_v<Condition, First, Rest...> = Condition<std::remove_cvref_t<First>>::value;
 
 /**
  * Concept for std::vector-like containers.
