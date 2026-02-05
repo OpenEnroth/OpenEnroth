4a6d895f02d2c077293afa770ce857a8aa6b8828
captainurist <73941350+captainurist@users.noreply.github.com>
2026-02-05 22:32:51 +0000

    Fix MacOS Tahoe icon

--- src/Bin/OpenEnroth/GenerateAll.sh
+++ src/Bin/OpenEnroth/GenerateAll.sh
@@ -2,3 +2,4 @@
 
 ./GenerateIcns.sh ../../../resources/images/OpenEnroth.png OpenEnroth.icns
 ./GenerateIco.sh ../../../resources/images/OpenEnroth.png OpenEnroth.ico
+./GenerateTahoeIcon.sh ../../../resources/images/OpenEnroth.png Assets.car
