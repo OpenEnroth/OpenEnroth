581d9233467fa3eb45e65e1f49b2f8fed4e2c0ea
Ensrick <truthisfact@gmail.com>
2026-03-15 21:17:40 -0500

    Deduplicate config-based commands against Lua defaults in debug menu

--- launch_personal.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Launch OpenEnroth from the personal branch with a separate userdata folder.
-REM Saves, config (openenroth.ini), and screenshots go to OpenEnroth_Personal
-REM instead of the default OpenEnroth folder in Saved Games.
-set EXE=%~dp0build\src\Bin\OpenEnroth\Debug\OpenEnroth.exe
-set USERPATH=C:\Users\danjo\Saved Games\OpenEnroth_Personal
-"%EXE%" --user-path "%USERPATH%"
