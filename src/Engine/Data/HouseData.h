f11aa0dcea24267feb550e8e0a702d732f33f05b
captainurist <73941350+captainurist@users.noreply.github.com>
2026-05-05 21:27:47 +0100

    Defaults for struct HouseData

--- src/Engine/Data/HouseData.h
+++ src/Engine/Data/HouseData.h
@@ -8,24 +8,24 @@
 #include "Engine/PartyEnums.h"
 
 struct HouseData {
-    HouseType uType;
-    uint16_t uAnimationID;
+    HouseType uType = HOUSE_TYPE_INVALID;
+    uint16_t uAnimationID = 0;
     std::string name;
     std::string pProprieterName;
     std::string pEnterText;
     std::string pProprieterTitle;
-    int16_t field_14;
-    int16_t _state;
-    int16_t _rep;
-    int16_t _per;
-    int16_t generation_interval_days;
-    int16_t field_1E;
-    float fPriceMultiplier; // shop price multiplier
-    float flt_24; // skills price multiplier
-    uint16_t uOpenTime;
-    uint16_t uCloseTime;
-    int16_t uExitPicID;
-    MapId uExitMapID;
-    QuestBit _quest_bit;
-    int16_t field_32;
+    int16_t field_14 = 0;
+    int16_t _state = 0;
+    int16_t _rep = 0;
+    int16_t _per = 0;
+    int16_t generation_interval_days = 0;
+    int16_t field_1E = 0;
+    float fPriceMultiplier = 0; // shop price multiplier
+    float flt_24 = 0; // skills price multiplier
+    uint16_t uOpenTime = 0;
+    uint16_t uCloseTime = 0;
+    int16_t uExitPicID = 0;
+    MapId uExitMapID = MAP_INVALID;
+    QuestBit _quest_bit = QBIT_INVALID;
+    int16_t field_32 = 0;
 };
