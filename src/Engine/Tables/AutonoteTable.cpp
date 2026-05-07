e04d567faaa1b9ba234be6cc319b1360de1ed12c
captainurist <73941350+captainurist@users.noreply.github.com>
2026-05-07 10:11:28 +0100

    New code for autonotes table parsing

--- src/Engine/Tables/AutonoteTable.cpp
+++ src/Engine/Tables/AutonoteTable.cpp
@@ -1,64 +1,36 @@
 #include "AutonoteTable.h"
 
-#include <cstring>
 #include <map>
 #include <string>
+#include <vector>
+
+#include "Library/Serialization/Serialization.h"
 
 #include "Utility/MapAccess.h"
 #include "Utility/Memory/Blob.h"
 #include "Utility/String/Ascii.h"
+#include "Utility/String/Split.h"
 #include "Utility/String/Transformations.h"
 
-std::array<AutonoteData, 196> pAutonoteTxt;
+// Expanded from 196 to 300 to accommodate empty entries in autonote.txt.
+std::array<AutonoteData, 300> pAutonoteTxt;
 
 void initializeAutonotes(const Blob &autonotes) {
+    // autonote.txt table structure: index | text (localized) | type (not localized).
     static const std::map<std::string, AutonoteType, ascii::NoCaseLess> autonoteTypeMap = {
         {"potion", AUTONOTE_POTION_RECIPE},
         {"stat", AUTONOTE_STAT_HINT},
         {"seer", AUTONOTE_SEER},
         {"obelisk", AUTONOTE_OBELISK},
         {"teacher", AUTONOTE_TEACHER},
+        {"misc", AUTONOTE_MISC}
     };
 
-    char *test_string;
-    unsigned char c;
-    bool break_loop;
-    unsigned int temp_str_len;
-    char *tmp_pos;
-    int decode_step;
-
-    std::string txtRaw(autonotes.str());
-    strtok(txtRaw.data(), "\r");
-
-    for (int i = 1; i < pAutonoteTxt.size(); ++i) {
-        test_string = strtok(NULL, "\r") + 1;
-        break_loop = false;
-        decode_step = 0;
-        do {
-            c = *(unsigned char *)test_string;
-            temp_str_len = 0;
-            while ((c != '\t') && (c > 0)) {
-                ++temp_str_len;
-                c = test_string[temp_str_len];
-            }
-            tmp_pos = test_string + temp_str_len;
-            if (*tmp_pos == 0) break_loop = true;
-            *tmp_pos = 0;
-            if (temp_str_len) {
-                switch (decode_step) {
-                    case 1:
-                        pAutonoteTxt[i].pText = removeQuotes(test_string);
-                        break;
-                    case 2:
-                        pAutonoteTxt[i].eType = valueOr(autonoteTypeMap, test_string, AUTONOTE_MISC);
-                        break;
-                }
-            } else {
-                break_loop = true;
-            }
-            ++decode_step;
-            test_string = tmp_pos + 1;
-        } while ((decode_step < 3) && !break_loop);
+    std::vector<std::string_view> tokens;
+    for (std::string_view line : split(autonotes.str()).by("\r\n").drop(1).skip("")) {
+        split(line).by('\t').resize(3, "").to(&tokens); // Truncated lines with just the index exist in the file.
+        int i = fromString<int>(tokens[0]);
+        pAutonoteTxt[i].pText = removeQuotes(tokens[1]);
+        pAutonoteTxt[i].eType = valueOr(autonoteTypeMap, tokens[2], AUTONOTE_MISC);
     }
 }
-
