7966829d3034ef393b116a49aa6c05ef9032fa79
captainurist <73941350+captainurist@users.noreply.github.com>
2026-05-09 12:58:06 +0100

    New parser for message scroll table

--- src/Engine/Tables/MessageScrollTable.cpp
+++ src/Engine/Tables/MessageScrollTable.cpp
@@ -1,46 +1,24 @@
 #include "MessageScrollTable.h"
 
-#include <cstring>
+#include <array>
 #include <string>
 
+#include "Library/Serialization/Serialization.h"
+
 #include "Utility/Memory/Blob.h"
+#include "Utility/String/Split.h"
 #include "Utility/String/Transformations.h"
 
 IndexedArray<std::string, ITEM_FIRST_MESSAGE_SCROLL, ITEM_LAST_MESSAGE_SCROLL> pMessageScrolls;
 
 void initializeMessageScrolls(const Blob &scrolls) {
-    char *test_string;
-    unsigned char c;
-    bool break_loop;
-    unsigned int temp_str_len;
-    char *tmp_pos;
-    int decode_step;
+    // scroll.txt table structure: item index | message text (localized) | scroll title (localized, not used) | (empty).
+    for (std::string_view line : split(scrolls.str()).by("\r\n").drop(1).skip("")) {
+        std::array<std::string_view, 2> tokens = split(line).by('\t');
+        if (tokens[0].empty())
+            continue; // Skip tab-only trailing lines.
 
-    std::string txtRaw(scrolls.str());
-    strtok(txtRaw.data(), "\r");
-    for (ItemId i : pMessageScrolls.indices()) {
-        test_string = strtok(NULL, "\r") + 1;
-        break_loop = false;
-        decode_step = 0;
-        do {
-            c = *(unsigned char *)test_string;
-            temp_str_len = 0;
-            while ((c != '\t') && (c > 0)) {
-                ++temp_str_len;
-                c = test_string[temp_str_len];
-            }
-            tmp_pos = test_string + temp_str_len;
-            if (*tmp_pos == 0) break_loop = true;
-            *tmp_pos = 0;
-            if (temp_str_len) {
-                if (decode_step == 1)
-                    pMessageScrolls[i] = removeQuotes(test_string);
-            } else {
-                break_loop = true;
-            }
-            ++decode_step;
-            test_string = tmp_pos + 1;
-        } while ((decode_step < 2) && !break_loop);
+        ItemId i = static_cast<ItemId>(fromString<int>(tokens[0]));
+        pMessageScrolls[i] = removeQuotes(tokens[1]);
     }
 }
-
