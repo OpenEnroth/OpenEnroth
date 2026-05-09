b1048a2c3e4a5b625eea1a7657486390fad88ef5
captainurist <73941350+captainurist@users.noreply.github.com>
2026-05-09 20:53:48 +0100

    Transition table parsing + minor cleanup (#2486)
    * Added another small check to Issue1331
    
    * Docs
    
    * TODO added
    
    * New code for transition table parsing

--- src/Engine/Tables/TransitionTable.cpp
+++ src/Engine/Tables/TransitionTable.cpp
@@ -1,48 +1,24 @@
 #include "TransitionTable.h"
 
-#include <cstring>
+#include <array>
+#include <cassert>
 #include <string>
 
+#include "Library/Serialization/Serialization.h"
+
 #include "Utility/Memory/Blob.h"
+#include "Utility/String/Split.h"
 #include "Utility/String/Transformations.h"
 
 std::array<std::string, 465> pTransitionStrings;
 
 void initializeTransitions(const Blob &transitions) {
-    char *test_string;
-    unsigned char c;
-    bool break_loop;
-    unsigned int temp_str_len;
-    char *tmp_pos;
-    int decode_step;
+    // trans.txt table structure: index | description (localized) | name (not localized, not used).
+    pTransitionStrings.fill({});
 
-    std::string txtRaw(transitions.str());
-    strtok(txtRaw.data(), "\r");
-
-    pTransitionStrings[0] = "";
-    for (int i = 1; i < pTransitionStrings.size(); ++i) {
-        test_string = strtok(NULL, "\r") + 1;
-        break_loop = false;
-        decode_step = 0;
-        do {
-            c = *(unsigned char *)test_string;
-            temp_str_len = 0;
-            while ((c != '\t') && (c > 0)) {
-                ++temp_str_len;
-                c = test_string[temp_str_len];
-            }
-            tmp_pos = test_string + temp_str_len;
-            if (*tmp_pos == 0) break_loop = true;
-            *tmp_pos = 0;
-            if (temp_str_len) {
-                if (decode_step == 1)
-                    pTransitionStrings[i] = removeQuotes(test_string);
-            } else {
-                break_loop = true;
-            }
-            ++decode_step;
-            test_string = tmp_pos + 1;
-        } while ((decode_step < 2) && !break_loop);
+    for (std::string_view line : split(transitions.str()).by("\r\n").drop(1).skip("")) {
+        std::array<std::string_view, 2> tokens = split(line).by('\t'); // Some rows have no description, so it defaults to "".
+        int i = fromString<int>(tokens[0]);
+        pTransitionStrings[i] = removeQuotes(tokens[1]);
     }
 }
-
