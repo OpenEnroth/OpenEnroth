ce6af7455db75c1c249c0596ef3762753eb18792
Peter Skelton <peter.r.skelton@gmail.com>
2026-07-27 21:58:05 +0100

    fix2524

--- src/Library/Binary/CommonSerialization.h
+++ src/Library/Binary/CommonSerialization.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cassert>
+#include <cstdint>
 #include <span>
 #include <array>
 #include <typeinfo>
--- src/Media/FFmpegBlobIoContext.h
+++ src/Media/FFmpegBlobIoContext.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 #include "Utility/Memory/Blob.h"
 #include "Utility/Streams/BlobInputStream.h"
 
