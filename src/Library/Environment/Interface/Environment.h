f1e332ce964670dc7d522726bd08ac1e217f092a
captainurist <73941350+captainurist@users.noreply.github.com>
2026-08-18 23:36:00 -0700

    State what a function returns first, and its encoding second

--- src/Library/Environment/Interface/Environment.h
+++ src/Library/Environment/Interface/Environment.h
@@ -8,9 +8,8 @@
 /**
  * Base class akin to `Platform` that provides an abstraction for the process's environment.
  *
- * Strings accepted by and returned from methods of this class are WTF8-encoded on Windows - the underlying wide
- * strings are converted to and from WTF8. On POSIX they are just byte strings that are passed through as-is - POSIX
- * doesn't guarantee UTF8 in paths, or anywhere else.
+ * Strings accepted by and returned from methods of this class are WTF8-encoded on Windows. On POSIX they are just
+ * byte strings that are passed through as-is - POSIX doesn't guarantee UTF8 in paths, or anywhere else.
  *
  * Why is this class not a part of `Platform`? Mainly for the following reasons:
  * - `Platform` handles an unrelated domain (UI and window management). Using a `NullPlatform` while still relying on
@@ -31,9 +30,9 @@ class Environment {
     /**
      * Windows-only function for querying the registry. Always returns an empty string on non-Windows systems.
      *
-     * @param path                      Registry path to query, WTF8-encoded on Windows.
-     * @return                          Value at the given path, WTF8-encoded on Windows, or an empty string in case
-     *                                  of an error.
+     * @param path                      Registry path to query. WTF8-encoded.
+     * @return                          Value at the given path, or an empty string in case of an error.
+     *                                  WTF8-encoded.
      */
     [[nodiscard]] virtual std::string queryRegistry(const std::string &path) const = 0;
 
@@ -41,12 +40,13 @@ class Environment {
      * Accessor for various system paths.
      *
      * @param path                      Path to get.
-     * @return                          Path, WTF8-encoded on Windows, or an empty string in case of an error.
+     * @return                          Requested path, or an empty string in case of an error. WTF8-encoded on
+     *                                  Windows, byte string on POSIX.
      */
     [[nodiscard]] virtual std::string path(EnvironmentPath path) const = 0;
 
     /**
-     * Same as `std::getenv`, but takes & returns WTF8-encoded keys and values on Windows.
+     * Same as `std::getenv`, but doesn't depend on the C locale.
      *
      * Note that on Windows `std::getenv` doesn't switch to UTF8 even if `UnicodeCrt` is used
      * (aka `std::setlocale(LC_ALL, ".UTF-8")`).
@@ -54,16 +54,20 @@ class Environment {
      * Returns an empty string for non-existent environment variables, and thus doesn't distinguish between empty and
      * non-existent values (and you shouldn't, either).
      *
-     * @param key                       Name of the environment variable to query, WTF8-encoded on Windows.
-     * @return                          Value of the environment variable, WTF8-encoded on Windows.
+     * @param key                       Name of the environment variable to query. WTF8-encoded on Windows, byte
+     *                                  string on POSIX.
+     * @return                          Value of the environment variable. WTF8-encoded on Windows, byte string on
+     *                                  POSIX.
      */
     [[nodiscard]] virtual std::string getenv(const std::string &key) const = 0;
 
     /**
-     * Same as POSIX `setenv(key, value, 1)`. Takes WTF8-encoded keys and values on Windows.
+     * Same as POSIX `setenv(key, value, 1)`.
      *
-     * @param key                       Name of the environment variable to set, WTF8-encoded on Windows.
-     * @param value                     Value of the environment variable, WTF8-encoded on Windows.
+     * @param key                       Name of the environment variable to set. WTF8-encoded on Windows, byte
+     *                                  string on POSIX.
+     * @param value                     Value of the environment variable. WTF8-encoded on Windows, byte string on
+     *                                  POSIX.
      */
     virtual void setenv(const std::string &key, const std::string &value) const = 0;
 };
