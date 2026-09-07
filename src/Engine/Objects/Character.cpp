ebd8ddb78e07b50578884b46ecdda99407f16781
captainurist <73941350+captainurist@users.noreply.github.com>
2026-09-06 22:56:21 -0700

    Rename matchedClass to baseClass in matchesAttackPreference

--- src/Engine/Objects/Character.cpp
+++ src/Engine/Objects/Character.cpp
@@ -6773,17 +6773,17 @@ void Character::Zero() {
 }
 
 bool Character::matchesAttackPreference(MonsterAttackPreference preference) const {
-    Class matchedClass = engine->config->gameplay.AttackPreferencesIncludePromotions.value() ? getTier1Class(classType) : classType;
+    Class baseClass = engine->config->gameplay.AttackPreferencesIncludePromotions.value() ? getTier1Class(classType) : classType;
     switch (preference) {
-    case ATTACK_PREFERENCE_KNIGHT:      return matchedClass == CLASS_KNIGHT;
-    case ATTACK_PREFERENCE_PALADIN:     return matchedClass == CLASS_PALADIN;
-    case ATTACK_PREFERENCE_ARCHER:      return matchedClass == CLASS_ARCHER;
-    case ATTACK_PREFERENCE_DRUID:       return matchedClass == CLASS_DRUID;
-    case ATTACK_PREFERENCE_CLERIC:      return matchedClass == CLASS_CLERIC;
-    case ATTACK_PREFERENCE_SORCERER:    return matchedClass == CLASS_SORCERER;
-    case ATTACK_PREFERENCE_RANGER:      return matchedClass == CLASS_RANGER;
-    case ATTACK_PREFERENCE_THIEF:       return matchedClass == CLASS_THIEF;
-    case ATTACK_PREFERENCE_MONK:        return matchedClass == CLASS_MONK;
+    case ATTACK_PREFERENCE_KNIGHT:      return baseClass == CLASS_KNIGHT;
+    case ATTACK_PREFERENCE_PALADIN:     return baseClass == CLASS_PALADIN;
+    case ATTACK_PREFERENCE_ARCHER:      return baseClass == CLASS_ARCHER;
+    case ATTACK_PREFERENCE_DRUID:       return baseClass == CLASS_DRUID;
+    case ATTACK_PREFERENCE_CLERIC:      return baseClass == CLASS_CLERIC;
+    case ATTACK_PREFERENCE_SORCERER:    return baseClass == CLASS_SORCERER;
+    case ATTACK_PREFERENCE_RANGER:      return baseClass == CLASS_RANGER;
+    case ATTACK_PREFERENCE_THIEF:       return baseClass == CLASS_THIEF;
+    case ATTACK_PREFERENCE_MONK:        return baseClass == CLASS_MONK;
     case ATTACK_PREFERENCE_MALE:        return uSex == SEX_MALE;
     case ATTACK_PREFERENCE_FEMALE:      return uSex == SEX_FEMALE;
     case ATTACK_PREFERENCE_HUMAN:       return GetRace() == RACE_HUMAN;
